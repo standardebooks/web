45078afb2ac7de96bc3058edcd6e4794f7cea6c0
Alex Cabal <alex@alexcabal.com>
2018-12-30 17:57:51 -0600

    Use let instead of var; code style updates

--- www/js/core.js
+++ www/js/core.js
@@ -1,10 +1,10 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function(){
 	if(hljs){
-		var blocks = document.querySelectorAll('code.html.full, code.css.full, figure code.html');
-		for (var i=0; i<blocks.length; i++) {
+		let blocks = document.querySelectorAll('code.html.full, code.css.full, figure code.html');
+		for(let i = 0; i < blocks.length; i++){
 			hljs.highlightBlock(blocks[i]);
-		};
+		}
 	}
 });
