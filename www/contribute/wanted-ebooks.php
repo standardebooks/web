0c5e05a953cbb31b3b412e55cb2af38405287a52
Alex Cabal <alex@alexcabal.com>
2019-02-16 18:25:28 -0600

    Update wanted ebooks

--- www/contribute/wanted-ebooks.php
+++ www/contribute/wanted-ebooks.php
@@ -26,9 +26,9 @@ require_once('Core.php');
 			<li>
 				<p><a href="https://www.gutenberg.org/ebooks/9807">Scarhaven Keep</a> by J. S. Fletcher</p>
 			</li>
-			<li>
+		<!-- 	<li>
 				<p><a href="https://www.gutenberg.org/ebooks/11666">The Conjure Woman</a> by Charles W. Chesnutt</p>
-			</li>
+			</li> -->
 			<li>
 				<p><a href="https://www.gutenberg.org/ebooks/50561">The Dark Other</a> by Stanley G. Weinbaum</p>
 			</li>
