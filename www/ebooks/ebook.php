46789303bdfa71c2d1b88c1f9a40d6ef18db3597
Alex Cabal <alex@alexcabal.com>
2019-08-16 15:57:15 -0500

    Fix stray closing <a> tag in collection link

--- www/ebooks/ebook.php
+++ www/ebooks/ebook.php
@@ -82,7 +82,7 @@ catch(\Exception $ex){
 					<? for($i = 0; $i < sizeof($ebook->Collections); $i++){ ?>
 					<a href="<?= $ebook->Collections[$i]->Url ?>"><?= Formatter::ToPlainText(preg_replace('/^The /ius', '', (string)$ebook->Collections[$i]->Name) ?? '') ?></a><? if(sizeof($ebook->Collections) > 2){ ?><? if($i == sizeof($ebook->Collections) - 2){ ?>, and <? }elseif($i != sizeof($ebook->Collections) - 1){ ?>, <? } ?><? }elseif($i == sizeof($ebook->Collections) - 2){ ?> and <? } ?>
 					<? } ?>
-				collection<? if(sizeof($ebook->Collections) > 1){ ?>s<? } ?></a>.</p>
+				collection<? if(sizeof($ebook->Collections) > 1){ ?>s<? } ?>.</p>
 			<? } ?>
 			<ul class="tags"><? foreach($ebook->Tags as $tag){ ?><li><a href="<?= $tag->Url ?>"><?= Formatter::ToPlainText($tag->Name) ?></a></li><? } ?></ul>
 		</aside>
