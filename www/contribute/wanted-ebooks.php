c0ac7380e3af3088d87c824e9566f211ea43d2bb
Alex Cabal <alex@alexcabal.com>
2019-09-17 19:13:48 -0500

    Update wanted ebooks list

--- www/contribute/wanted-ebooks.php
+++ www/contribute/wanted-ebooks.php
@@ -137,9 +137,9 @@ require_once('Core.php');
 			<li>
 				<p><a href="https://www.gutenberg.org/ebooks/4057">Marius the Epicurean</a> by Walter Pater (<a href="https://www.gutenberg.org/ebooks/4058">Vol. 2</a>; both as a single production)</p>
 			</li>
-			<li>
+			<!-- <li>
 				<p><a href="https://www.gutenberg.org/ebooks/165">McTeague</a> by Frank Norris</p>
-			</li>
+			</li> -->
 			<li>
 				<p><a href="https://www.gutenberg.org/ebooks/24353">Wired Love</a> by Ella Cheever Thayer</p>
 			</li>
