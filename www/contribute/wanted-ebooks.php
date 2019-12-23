d827f786309b7536023b83d7602aeab46290a5d9
Alex Cabal <alex@alexcabal.com>
2019-12-23 16:07:45 -0600

    Update wanted ebooks list

--- www/contribute/wanted-ebooks.php
+++ www/contribute/wanted-ebooks.php
@@ -173,9 +173,6 @@ require_once('Core.php');
 			<!-- <li>
 				<p><a href="https://www.gutenberg.org/ebooks/2946">Howards End</a> by EM Forster</p>
 			</li> -->
-			<!-- <li>
-				<p><a href="https://www.gutenberg.org/ebooks/1400">Great Expectations</a> by Charles Dickens</p>
-			</li> -->
 			<li>
 				<p><a href="https://www.gutenberg.org/ebooks/54672">The Indiscreet Toys</a> by Denis Diderot</p>
 			</li>
