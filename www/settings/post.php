71664246c0dac09e911e911b0e4f9a897200e24a
Alex Cabal <alex@alexcabal.com>
2021-01-02 17:47:20 -0600

    Update setcookie() to use PHP 7.2 signature

--- www/settings/post.php
+++ www/settings/post.php
@@ -9,10 +9,10 @@ if($colorScheme !== 'dark' && $colorScheme !== 'light' && $colorScheme !== 'auto
 
 if($colorScheme == 'auto'){
 	// Delete the cookie; auto is the default
-	setcookie('color-scheme', null, ['expires' => 0, 'path' => '/', 'secure' => true, 'httponly' => true]);
+	setcookie('color-scheme', null, 0, '/', '', true, true);
 }
 else{
-	setcookie('color-scheme', $colorScheme, ['expires' => strtotime('+10 years'), 'path' => '/', 'secure' => true, 'httponly' => true]);
+	setcookie('color-scheme', $colorScheme, strtotime('+10 years'), '/', '', true, true);
 }
 
 // HTTP 303, See other
