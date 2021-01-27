eb02e6ee9e4195e800bcef27983f5dc692fcab03
Alex Cabal <alex@alexcabal.com>
2021-01-27 16:01:16 -0600

    Remove unused template file and add outdated watermarks for manual

--- templates/OldManualVersionWarning.php
+++
@@ -1,7 +0,0 @@
-<? if(Manual::GetRequestedVersion() !== Manual::GetLatestVersion()){ ?>
-<aside class="alert">
-	<p>This is an outdated version of the manual that is no longer current.</p>
-	<p><a href="/manual/latest">Read the latest version instead.</a></p>
-</aside>
-<? } ?>
-
