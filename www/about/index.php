b039ebfa7d1dfd375b93e243d249f3dab71f1d00
Alex Cabal <alex@alexcabal.com>
2021-02-16 11:30:15 -0600

    Update masthead

--- www/about/index.php
+++ www/about/index.php
@@ -127,7 +127,7 @@ require_once('Core.php');
 					</li>
 					<li>
 						<img src="/images/masthead/portrait.svg" role="presentation" alt=""/>
-						<p>Anonymous × 1</p>
+						<p>Anonymous × 2</p>
 					</li>
 					<? if(false){ ?>
 					<li>
