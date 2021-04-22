1743b42f56c7fd05908dfa85746cbaaaf003a613
Alex Cabal <alex@alexcabal.com>
2021-04-22 18:48:18 -0500

    Add LC3 information

--- templates/Footer.php
+++ templates/Footer.php
@@ -26,8 +26,8 @@
 				<a href="/rss/new-releases">New Releases RSS Feed</a>
 			</li>
 		</ul>
-		<p>Content produced by or for the Standard Ebooks project is dedicated to the <a href="https://en.wikipedia.org/wiki/Public_domain">public domain</a> via the <a href="https://creativecommons.org/publicdomain/zero/1.0/">CC0 1.0 Universal Public Domain Dedication</a>.</p>
-		<p>Content not produced by or for the Standard Ebooks project but displayed on this website may be subject to copyright.</p>
+		<p>Content produced by or for Standard Ebooks L<sup>3</sup>C is dedicated to the <a href="https://en.wikipedia.org/wiki/Public_domain">public domain</a> via the <a href="https://creativecommons.org/publicdomain/zero/1.0/">CC0 1.0 Universal Public Domain Dedication</a>.</p>
+		<p>Content not produced by or for Standard Ebooks L<sup>3</sup>C but displayed on this website may be subject to copyright.</p>
 		<p><a href="/">Standard Ebooks</a></p>
 	</footer>
 </body>
