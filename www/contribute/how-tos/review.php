46441e170c751dab5db9c354e77870eaa4e4184e
Matt Chan <thewchan@gmail.com>
2021-07-22 16:34:34 -0400

    Add intro paragraph

--- www/contribute/how-tos/review.php
+++ www/contribute/how-tos/review.php
@@ -1,9 +1,10 @@
 <?
 require_once('Core.php');
-?><?= Template::Header(['title' => 'How to Produce a Shakespeare Play', 'manual' => true, 'highlight' => 'contribute', 'description' => 'A guide to producing any play by William Shakespeare.']) ?>
+?><?= Template::Header(['title' => 'How to review a production before it is published', 'manual' => true, 'highlight' => 'contribute', 'description' => 'A guide to proofread and review an ebook production before it is published.']) ?>
 <main class="manual">
 	<article class="step-by-step-guide">
 		<h1>How to review a production before it is published</h1>
+		<p>After an ebook production is completed, it must go through rounds of review before it is published to ensure appropriate the appropriate production quality is assured. Reviewers are assigned by the <a href="/about#editor-in-chief">editor-in-chief</a>, and may be a member of the <a href="/about#editors">editorial staff</a>, or an experienced Standard Ebooks producer. Below is a helpful step-by-step checklist for reviewers of ebook productions. The checklist is by no means exhaustive, but serves as a good starting point of the proofreading process. Reviewers to keep in mind the standards enumerated in the <a href="/manual">Manual of Style</a>, and note any discrepancies not listed below.</p>
 	</article>
 </main>
 <?= Template::Footer() ?>
