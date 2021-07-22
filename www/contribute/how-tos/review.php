54e3fd6b1f2e6f5f0733f6a130cab96347fd2629
Matt Chan <thewchan@gmail.com>
2021-07-22 16:21:00 -0400

    Add skelton structure in review.php

---
+++ www/contribute/how-tos/review.php
@@ -0,0 +1,8 @@
+<?
+require_once('Core.php');
+?><?= Template::Header(['title' => 'How to Produce a Shakespeare Play', 'manual' => true, 'highlight' => 'contribute', 'description' => 'A guide to producing any play by William Shakespeare.']) ?>
+<main class="manual">
+	<article class="step-by-step-guide">
+		<h1>How to review  a production before it is published</h1>
+	</article>
+</main>
