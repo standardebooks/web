2b6d5ce770f6ac50952ce5685eb49ad3b50b5e7e
Matt Chan <thewchan@gmail.com>
2021-07-22 17:27:44 -0400

    Added step 5 and 6

--- www/contribute/how-tos/review.php
+++ www/contribute/how-tos/review.php
@@ -8,7 +8,7 @@ require_once('Core.php');
 		<ol>
 			<li>
 				<h2>Step 1</h2>
-				<p>Run <code class="bash"><b>se</b> typogrify .</code> at the root of the project directory. The <code>typogrify</code> command is almost always correct, but sometimes not all changes it makes should be accepted. To go over the changes <code>typogrify</code> may have made, run the following <code><b>git</b></code> command to also highlight changes made to invisible or hard to differentiate Unicode characters:</p>
+				<p>Before any review step, ensure the most recent version of the SE toolset is installed. Run <code class="bash"><b>se</b> typogrify .</code> at the root of the project directory. The <code>typogrify</code> command is almost always correct, but sometimes not all changes it makes should be accepted. To go over the changes <code>typogrify</code> may have made, run the following <code><b>git</b></code> command to also highlight changes made to invisible or hard to differentiate Unicode characters:</p>
 				<code class="terminal"><b>git</b> diff -U0 --word-diff-regex=.</code>
 			</li>
 			<li>
@@ -17,11 +17,19 @@ require_once('Core.php');
 			</li>
 			<li>
 				<h2>Step 3</h2>
-				<p>Run <code class="bash"><b>se</b> semanticate .</code> at the root of the project directory. Unlike <code>typogrify</code> or <code>modernize-spelling</code>, <code>semanticate</code> is more prone to error or false positives. Judicious use of the <code><b>git</b> diff</code> command listed in Step 1 would be needed to prevent any unwanted changes.</p>
+				<p>Run <code class="bash"><b>se</b> semanticate .</code> at the root of the project directory. Unlike <code>typogrify</code> or <code>modernize-spelling</code>, <code>semanticate</code> is more prone to error or false positives. Judicious use of the <code class="bash"><b>git</b> diff</code> command listed in Step 1 would be needed to prevent any unwanted changes.</p>
 			</li>
 			<li>
 				<h2>Step 4</h2>
-				<p>Run <code class="bash"><b>se</b> clean .</code> at the root of the project directory. Ideally the producer of the ebook would have ran this multiple times during their production process. However, since changes may have been made since then by the reviewer and stylistic deviations may be been inadvertently introduced, this will clean those potential errors up. After each step so far, it is recommended to use the <code><b>git</b> diff</code> commanded listed in Step 1 to review all changes.</p>
+				<p>Run <code class="bash"><b>se</b> clean .</code> at the root of the project directory. Ideally the producer of the ebook would have ran this multiple times during their production process. However, since changes may have been made since then by the reviewer and stylistic deviations may be been inadvertently introduced, this will clean those potential errors up. After each step so far, it is recommended to use the <code class="bash"><b>git</b> diff</code> commanded listed in Step 1 to review all changes.</p>
+			</li>
+			<li>
+				<h2>Step 5</h2>
+				<p>Run both <code class="bash"><b>se</b> find-mismatched-dashes .</code> and <code class="bash"><b>se</b> find-mismatched-diacritics .</code> at the root of the project directory. Either of these tools will make actual changes to the project, but will list words that have inconsistent application of dashes/diacritics. Check with the language authority listed in the <a href="/manual/latest/single-page#8.2.9">Manual</a> to decide on the recommendations.</p>
+			</li>
+			<li>
+				<h2>Step 6</h2>
+				<p>Run <code class="bash"><b>se</b> build-title .</code>, <code class="bash"><b>se</b> build-toc .</code> and <code class="bash"><b>se</b> build-images .</code> at the root of the project directory. No changes should be made by these tools if the producer correctly generated the title page, table of content, and image files. Any discrepancies can be highlighted and investigated via the <code class="bash"><b>git</b> diff</code> listed in Step 1.</p>
 			</li>
 		</ol>
 	</article>
