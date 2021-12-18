9646612cfc05d36f88666a74f6b357b78a9cb8c4
Alex Cabal <alex@alexcabal.com>
2021-12-17 20:49:48 -0600

    Update wanted ebooks list

--- www/contribute/wanted-ebooks.php
+++ www/contribute/wanted-ebooks.php
@@ -122,9 +122,6 @@ require_once('Core.php');
 			<!-- <li>
 				<p><a href="https://www.gutenberg.org/ebooks/78">Tarzan of the Apes</a> by Edgar Rice Burroughs</p>
 			</li> -->
-			<!-- <li>
-				<p><a href="https://www.gutenberg.org/ebooks/81">The Return of Tarzan</a> (Tarzan 2) by Edgar Rice Burroughs</p>
-			</li> -->
 			<li>
 				<p><a href="https://www.gutenberg.org/ebooks/85">The Beasts of Tarzan</a> (Tarzan 3) by Edgar Rice Burroughs</p>
 			</li>
