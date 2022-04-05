00f85934d96986633c51747b2aaebce5edc2cd27
Alex Cabal <alex@alexcabal.com>
2022-04-05 10:38:13 -0500

    Clarify proofreading section in step by step guide

--- www/contribute/producing-an-ebook-step-by-step.php
+++ www/contribute/producing-an-ebook-step-by-step.php
@@ -571,7 +571,21 @@ proceed to seal up my confession, I bring the life of that unhappy Henry Jekyll 
 					</li>
 				</ul>
 				<p>Now, transfer the ebook to your ereader and start a cover-to-cover proofread.</p>
-				<p>It’s extremely common for transcriptions sourced from Project Gutenberg to have various typos and formatting errors (like missing italics), and it’s also not uncommon for one of Standard Ebook’s tools to make the wrong guess about things like a closing quotation mark somewhere. As you proofread, mark any obvious, or possible but not obvious, errors so that you can compare them with the page scans you found earlier. Keep an eye out for things that we may have to adjust in order to make the text conform to the <a href="/manual/latest/8-typography">Typography section of the <abbr class="acronym">SEMoS</abbr></a>.</p>
+				<h3>What do we mean by “proofreading”?</h3>
+				<p>“Proofreading” means a close reading of the text to try to spot any transcription errors or issues which the <abbr class="acronym">SEMoS</abbr> says we must update. It’s typically <em>not</em> a line-by-line comparison to the page scans—that work was already done by the initial transcriber. Rather, proofreading is reading the book as you would any other book, but with careful attention to possible problems in the transcription or in your production.</p>
+				<h3>Common proofreading issues</h3>
+				<ul>
+					<li><p><b>Missing or incorrect punctuation.</b> Often <abbr class="initialism">O.C.R.</abbr> software misses small punctuation marks like commas and periods. Does a sentence sound awkward, as if it was missing a comma? Is a period obviously missing between sentences? Mark it and check it against the page scans.</p></li>
+					<li><p><b>Missing formatting.</b> Transcribers often remove formatting like blockquotes or italics. Is there a section in the book that looks like it should be styled as a blockquote, like verse or a letter? Are characters speaking emphatically, but without italics? Mark these cases to compare against the page scans to see if formatting has to be restored.</p></li>
+					<li><p><b>Missing thought or paragraph breaks.</b> Is a paragraph unusually long? Does a scene change occur without <code class="html"><span class="p">&lt;</span><span class="nt">hr</span>/<span class="p">&gt;</span></code>? They might have been lost during transcription.</p></li>
+					<li><p><b>Errors caused by the S.E. toolset.</b> Tools like <code class="bash"><b>se</b> british2american</code> or even <code class="bash"><b>se</b> typogrify</code> can cause unexpected typography errors like quotation marks curled in the wrong direction, or dashes spaced incorrectly.</p></li>
+					<li><p><b>Archaic spellings.</b> Is a particular word spelled in a surprising way? Mark it to check if it should be modernized.  The <a href="https://books.google.com/ngrams/">Google Books Ngram Viewer</a> is a great tool to get an idea of whether a word used to be spelled one way, but isn’t spelled that way anymore. Remember to change spellings in their own commits, prefaced with <code class="html">[Editorial]</code>!</p></li>
+				</ul>
+				<p>There are some things that you don’t have to worry much about when proofreading:</p>
+				<ul>
+					<li><p><b>Spelling errors.</b> Actual spelling errors are very rare. If a word appears to be misspelled, it’s worth it to check the page scans, but such cases are often done on purpose by the author, or using a older spelling, or are spelled differently in en-US vs. en-GB.</p></li>
+					<li><p><b>Keeping a 100% faithful representation of a print page layout.</b> Sometimes books have complicated page layouts in print. But ebooks are not the same as print books, with the most important distinction being that there is no “page” to align items to. So, we’re not so concerned with maintaining a pixel-perfect reproduction of print layouts; rather, we wish to <em>adapt</em> print layouts as best we can to the ebook medium.</p></li>
+				</ul>
 				<aside class="tip">
 					<p>If you’re using a transcription from Project Gutenberg as the base for this ebook, you may wish to report typos you’ve found to them, so that they can correct their copy. <a href="/contribute/report-errors-upstream">Instructions for how to do so are here.</a></p>
 				</aside>
