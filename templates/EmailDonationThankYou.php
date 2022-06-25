86ae801f846530e1d64dd83e5d806b5f28420212
Alex Cabal <alex@alexcabal.com>
2022-06-25 11:25:43 -0500

    Tweak email template

--- templates/EmailDonationThankYou.php
+++ templates/EmailDonationThankYou.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 	<p>Hello,</p>
-	<p>I wanted to thank you personally for your recent donation to Standard Ebooks. Your donation will go towards continuing our mission of producing and distributing high-quality ebooks that are free of cost and free of copyright restrictions. Donations like yours help ensure that the world's literature is available in beautiful editions made for the digital age.</p>
+	<p>I wanted to thank you personally for your donation to Standard Ebooks. Your donation will go towards continuing our mission of producing and distributing high-quality ebooks that are free of cost and free of copyright restrictions. Donations like yours help ensure that the world's literature is available in beautiful editions made for the digital age.</p>
 	<p>Please don't hesitate to contact me if you have questions or suggestions. Thanks again for your donation, and for supporting the literate arts!</p>
 	<footer style="margin-top: 2em;">
 		<p>Alex Cabal</p>
