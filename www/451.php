fc0b69176c7af9c9afc98b46aae285013af1d472
Alex Cabal <alex@alexcabal.com>
2022-07-24 18:32:35 -0500

    Update 451 page with new art

--- www/451.php
+++ www/451.php
@@ -8,12 +8,12 @@ require_once('Core.php');
 			<h1>This Ebook Is No Longer Available</h1>
 			<h2>This is 451 error.</h2>
 		</hgroup>
-		<picture data-caption="The Course of Empire: Destruction. Thomas Cole, 1836">
-			<source srcset="/images/the-course-of-empire-destruction@2x.avif 2x, /images/the-course-of-empire-destruction.avif 1x" type="image/avif"/>
-			<source srcset="/images/the-course-of-empire-destruction@2x.jpg 2x, /images/the-course-of-empire-destruction.jpg 1x" type="image/jpg"/>
-			<img src="/images/the-course-of-empire-destruction@2x.jpg" alt="A classical city is aflame as people scramble in the foreground."/>
+		<picture data-caption="The Preaching of St. Paul at Ephesus. Eustache Le Sueur, 1649">
+			<source srcset="/images/the-preaching-of-st-paul-at-ephesus@2x.avif 2x, /images/the-preaching-of-st-paul-at-ephesus.avif 1x" type="image/avif"/>
+			<source srcset="/images/the-preaching-of-st-paul-at-ephesus@2x.jpg 2x, /images/the-preaching-of-st-paul-at-ephesus.jpg 1x" type="image/jpg"/>
+			<img src="/images/the-preaching-of-st-paul-at-ephesus@2x.jpg" alt="A classical city is aflame as people scramble in the foreground."/>
 		</picture>
-		<p>This ebook is no longer available, due to legal reasons. This may occur if the book was erroneously thought to be in the U.S. public domain, but new evidence changed that determination.</p>
+		<p>This ebook is no longer available, due to legal reasons. This may occur if the book was thought to be in the U.S. public domain, but new evidence changed that determination.</p>
 	</section>
 </main>
 <?= Template::Footer() ?>
