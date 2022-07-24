3b62424df7ffec9b14b9aa329d3b214d90eb4e51
Alex Cabal <alex@alexcabal.com>
2022-07-24 14:29:15 -0500

    Tweak bulk downloads index page wording

--- www/bulk-downloads/index.php
+++ www/bulk-downloads/index.php
@@ -15,7 +15,7 @@ if($GLOBALS['User'] !== null && $GLOBALS['User']->Benefits->CanBulkDownload){
 			<source srcset="/images/the-shop-of-the-bookdealer@2x.jpg 2x, /images/the-shop-of-the-bookdealer.jpg 1x" type="image/jpg"/>
 			<img src="/images/the-shop-of-the-bookdealer@2x.jpg" alt="A gentleman in regency-era dress buys books from a bookseller."/>
 		</picture>
-		<p><a href="/about#patrons-circle">Patrons circle members</a> get convenient access to zip files containing collections of different categories of ebooks. You can <a href="/donate#patrons-circle">join the Patrons Circle</a> with a small donation in support of our continuing mission to create free, beautiful digital literature, and download these collections files too.</p>
+		<p>Bulk downloads allow you to conveniently download zip files of entire collections of ebooks, in various ebook formats. Access to our bulk downloads is a benefit of <a href="/about#patrons-circle">Patrons circle membership</a>. Individuals can <a href="/donate#patrons-circle">join the Patrons Circle</a> with a small donation in support of our continuing mission to create free, beautiful digital literature.</p>
 		<ul>
 			<li>
 				<p><a href="/bulk-downloads/subjects">Downloads by subject</a></p>
