19b5aaab271c721157add8b06f0b2d12bebe40d6
Alex Cabal <alex@alexcabal.com>
2022-09-06 09:51:52 -0500

    Update wanted ebooks list

--- www/contribute/wanted-ebooks.php
+++ www/contribute/wanted-ebooks.php
@@ -383,9 +383,9 @@ require_once('Core.php');
 			<li>
 				<p><a href="https://www.gutenberg.org/ebooks/59752">The Moon Maid</a> (Moon 1) by Edgar Rice Burroughs (Note: We want the 1926 edition which is expanded, but requires transcription)</p>
 			</li>
-			<li>
+			<!-- <li>
 				<p><a href="https://www.gutenberg.org/ebooks/3335">Theodore Roosevelt: An Autobiography</a> by Theodore Roosevelt</p>
-			</li>
+			</li> -->
 			<li>
 				<p><a href="https://www.gutenberg.org/ebooks/11746">Through the Brazilian Wilderness</a> by Theodore Roosevelt (Transcription is missing italics and accents)</p>
 			</li>
