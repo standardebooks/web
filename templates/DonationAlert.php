52f47c828bf4e8b55bad86cbd5520fe5517a9653
Alex Cabal <alex@alexcabal.com>
2022-11-28 13:43:47 -0600

    Serve <div> instead of <aside> to Kindle browser to avoid it rendering as an undismissable popup

--- templates/DonationAlert.php
+++ templates/DonationAlert.php
@@ -1,6 +1,16 @@
-<? if($GLOBALS['User'] === null){ ?>
-<aside class="donation">
+<? if($GLOBALS['User'] === null){
+
+	// The Kindle browsers renders <aside> as an undismissable popup. Serve a <div> to Kindle instead. See https://github.com/standardebooks/web/issues/204
+	$element = 'aside';
+
+	if(stripos($_SERVER['HTTP_USER_AGENT'] ?? '', 'kindle') !== false){
+		$element = 'div';
+	}
+?>
+
+<<?= $element ?> class="donation">
 	<p>We rely on your support to help us keep producing beautiful, free, and unrestricted editions of literature for the digital age.</p>
 	<p>Will you <a href="/donate">support our efforts with a donation</a>?</p>
-</aside>
+</<?= $element ?>>
+
 <? } ?>
