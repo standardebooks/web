ae4c66a314cb44cc1c794fa2c516e115244221b5
Weijia Cheng <weijia05@yahoo.com>
2023-02-19 06:12:58 -0800

    Fix broken comment on wanted ebooks

--- www/contribute/wanted-ebooks.php
+++ www/contribute/wanted-ebooks.php
@@ -14,9 +14,9 @@ require_once('Core.php');
 		<p>If nothing on the list below interests you, you can pitch us something else you’d like to work on.</p>
 		<p>First productions should be on the shorter side (less than 100,000 words maximum) and without too many complex formatting issues like illustrations, significant endnotes, letters, poems, etc. Most short plain fiction novels fall in this category.</p>
 		<ul>
-			<!-- <li>
-				<p><a href="https://www.gutenberg.org/ebooks/41981">The Jewels of Aptor</a> by Samuel R. Delaney</p>!--patron--
-			</li> -->
+			<!-- <li> -->
+				<!-- <p><a href="https://www.gutenberg.org/ebooks/41981">The Jewels of Aptor</a> by Samuel R. Delaney</p> patron -->
+			<!-- </li> -->
 			<!-- <li>
 				<p><a href="https://www.gutenberg.org/ebooks/1480">Tom Brown’s School Days</a> by Thomas Hughes</p>
 			</li> -->
