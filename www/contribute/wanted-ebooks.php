7fd0fb7befe85f87754b4bc615ce7dff0c1a6f29
Alex Cabal <alex@alexcabal.com>
2023-03-15 17:48:36 -0500

    Update wanted ebooks list

--- www/contribute/wanted-ebooks.php
+++ www/contribute/wanted-ebooks.php
@@ -71,9 +71,6 @@ require_once('Core.php');
 			<li>
 				<p><a href="https://www.gutenberg.org/ebooks/46558">The Demi-gods</a> by James Stephens</p>
 			</li>
-			<!-- <li>
-				<p><a href="https://www.gutenberg.org/ebooks/37332">A Little Princess</a> by Francis Hodgson Burnett</p>
-			</li> -->
 			<li>
 				<p><a href="https://www.gutenberg.org/ebooks/149">Beyond Thirty aka. The Lost Continent</a> (Beyond 1) by Edgar Rice Burroughs</p>
 			</li>
