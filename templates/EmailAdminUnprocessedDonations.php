2dd89c310f104f7120d0e737fcac84627b042eff
Alex Cabal <alex@alexcabal.com>
2023-05-15 12:36:02 -0500

    Add unprocessed pending payments watchdog

---
+++ templates/EmailAdminUnprocessedDonations.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Unprocessed donations in database</title>
+</head>
+<body>
+	<p>There are unprocessed donations in the database.</p>
+</body>
+</html>
---
+++ templates/EmailAdminUnprocessedDonationsText.php
@@ -0,0 +1,1 @@
+There are unprocessed donations in the database.
