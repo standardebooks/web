c792bdf94972d2bb9622a02d3ad634fbf25941a4
Mike Colagrosso <colagrosso@users.noreply.github.com>
2023-06-10 17:00:53 -0600

    Initial Artwork and Artist classes

---
+++ lib/Artwork.php
@@ -0,0 +1,148 @@
+<?
+use Safe\DateTime;
+
+/**
+ * @property string $UrlName
+ * @property array<ArtworkTag> $ArtworkTags
+ * @property Artist $Artist
+ */
+class Artwork extends PropertiesBase{
+	public $Name;
+	public $ArtworkId;
+	public $CompletedYear;
+	public $ImageFilesystemPath;
+	public $Created;
+	public $Status;
+	protected $_UrlName;
+	protected $_ArtworkTags = null;
+	protected $_Artist = null;
+
+	// *******
+	// GETTERS
+	// *******
+
+	/**
+	 * @return string
+	 */
+	protected function GetUrlName(): string{
+		if($this->_UrlName === null){
+			$this->_UrlName = Formatter::MakeUrlSafe($this->Name);
+		}
+
+		return $this->_UrlName;
+	}
+
+	/**
+	 * @return array<ArtworkTag>
+	 */
+	protected function GetArtworkTags(): array{
+		if($this->_ArtworkTags === null){
+			$this->_ArtworkTags = Db::Query('
+							SELECT t.*
+							from Tags t
+							inner join ArtworkTags at using (TagId)
+							where ArtworkId = ?
+						', [$this->ArtworkId], 'ArtworkTag');
+		}
+
+		return $this->_ArtworkTags;
+	}
+
+	/**
+	 * @return Artist
+	 */
+	protected function GetArtist(): Artist{
+		if($this->_Artist === null){
+			$result = Db::Query('
+					SELECT *
+					from Artists
+					where ArtistId = ?
+				', [$this->_Artist->ArtistId], 'Artist');
+			if(sizeof($result) == 0){
+				throw new Exceptions\InvalidArtistException();
+			}
+			$this->_Artist = $result[0];
+		}
+
+		return $this->_Artist;
+	}
+
+	// *******
+	// METHODS
+	// *******
+	protected function Validate(): void{
+		$error = new Exceptions\ValidationException();
+
+		if($this->Artist === null){
+			$error->Add(new Exceptions\InvalidArtworkException());
+		}
+
+		if($this->Name === null || strlen($this->Name) === 0){
+			$error->Add(new Exceptions\InvalidArtworkException());
+		}
+
+		if($this->UrlName === null || strlen($this->UrlName) === 0){
+			$error->Add(new Exceptions\InvalidArtworkException());
+		}
+
+		if($this->CompletedYear === null || strlen($this->CompletedYear) === 0){
+			$error->Add(new Exceptions\InvalidArtworkException());
+		}
+
+		if($this->ImageFilesystemPath === null || strlen($this->ImageFilesystemPath) === 0){
+			$error->Add(new Exceptions\InvalidArtworkException());
+		}
+
+		if($this->Status !== null && !in_array($this->Status, ['unverified', 'approved', 'declined', 'in_use'])){
+			$error->Add(new Exceptions\InvalidArtworkException());
+		}
+
+		if($this->ArtworkTags !== null && count($this->_ArtworkTags) > 1000){
+			$error->Add(new Exceptions\InvalidArtworkException());
+		}
+
+		if($error->HasExceptions){
+			throw $error;
+		}
+	}
+
+	public function Create(): void{
+		$this->Validate();
+		$this->Created = new DateTime();
+		Db::Query('
+			INSERT into Artworks (ArtistId, Name, UrlName, CompletedYear, ImageFilesystemPath, Created)
+			values (?,
+			        ?,
+			        ?,
+			        ?,
+			        ?,
+			        ?)
+		', [$this->Artist->ArtistId, $this->Name, $this->UrlName, $this->CompletedYear, $this->ImageFilesystemPath, $this->Created]);
+
+		$this->ArtworkId = Db::GetLastInsertedId();
+
+		if($this->_ArtworkTags !== null){
+			for ($i = 0; $i < count($this->ArtworkTags); $i++){
+				Db::Query('
+					INSERT into ArtworkTags (ArtworkId, TagId)
+					values (?,
+					        ?)
+				', [$this->ArtworkId, $this->ArtworkTags[$i]->TagId]);
+			}
+		}
+	}
+
+	public function Delete(): void{
+		Db::Query('
+			DELETE
+			from ArtworkTags
+			where ArtworkId = ?
+		', [$this->ArtworkId]);
+
+		Db::Query('
+			DELETE
+			from Artworks
+			where ArtworkId = ?
+		', [$this->ArtworkId]);
+	}
+}
