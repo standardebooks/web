1f3b66144d18b487ecfcf7388a576128facc2430
Mike Colagrosso <colagrosso@users.noreply.github.com>
2023-06-17 12:35:07 -0600

    Add IsMatchingDonation to Payments

--- config/sql/se/Payments.sql
+++ config/sql/se/Payments.sql
@@ -7,6 +7,7 @@ CREATE TABLE `Payments` (
   `Amount` decimal(7,2) unsigned NOT NULL,
   `Fee` decimal(7,2) unsigned NOT NULL DEFAULT 0.00,
   `IsRecurring` tinyint(1) unsigned NOT NULL,
+  `IsMatchingDonation` tinyint(1) unsigned NOT NULL,
   PRIMARY KEY (`PaymentId`),
   KEY `index2` (`UserId`,`Amount`,`Created`,`IsRecurring`)
 ) ENGINE=InnoDB AUTO_INCREMENT=828 DEFAULT CHARSET=utf8mb4;
