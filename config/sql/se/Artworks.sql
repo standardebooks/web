2a176c49e4170550ab37dbfd854fedb61db07fc7
Mike Colagrosso <colagrosso@users.noreply.github.com>
2023-06-28 17:09:46 -0600

    Artwork review queue for admins (#240)
    Artwork review queue for admins
    
    Progress on #234

--- config/sql/se/Artworks.sql
+++ config/sql/se/Artworks.sql
@@ -12,5 +12,6 @@ CREATE TABLE `Artworks` (
   `PublicationYearPage` varchar(255) NULL,
   `CopyrightPage` varchar(255) NULL,
   `ArtworkPage` varchar(255) NULL,
-  PRIMARY KEY (`ArtworkId`)
+  PRIMARY KEY (`ArtworkId`),
+  KEY `index1` (`Status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
