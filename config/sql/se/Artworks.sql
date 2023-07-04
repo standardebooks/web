0e9c53730d277d99d1e1a80bf1a8e3eaad09c3b4
Mike Colagrosso <colagrosso@users.noreply.github.com>
2023-07-04 01:38:41 -0600

    Add EbookWwwFilesystemPath to point to in_use ebook

--- config/sql/se/Artworks.sql
+++ config/sql/se/Artworks.sql
@@ -12,6 +12,7 @@ CREATE TABLE `Artworks` (
   `PublicationYearPage` varchar(255) NULL,
   `CopyrightPage` varchar(255) NULL,
   `ArtworkPage` varchar(255) NULL,
+  `EbookWwwFilesystemPath` varchar(255) NULL,
   PRIMARY KEY (`ArtworkId`),
   KEY `index1` (`Status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- templates/ArtworkList.php
+++ templates/ArtworkList.php
@@ -16,7 +16,7 @@ $artworks = $artworks ?? [];
 		<p>Artist: <span class="author" typeof="schema:Person" property="schema:name"><?= Formatter::ToPlainText($artwork->Artist->Name) ?></span></p>
 		<div>
 			<p>Year completed: <? if ($artwork->CompletedYear === null){ ?>(unknown)<? }else{ ?><?= $artwork->CompletedYear ?><? if($artwork->CompletedYearIsCirca){ ?> (circa)<? } ?><? } ?></p>
-			<p>Status: <? if($artwork->Status === "approved"){ ?>Approved<? }else if($artwork->Status === "in_use"){ ?>In use by <? } ?></p>
+			<p>Status: <? if($artwork->Status === "approved"){ ?>Approved<? }else if($artwork->Status === "in_use"){ ?>In use by <a href="<?= $artwork->Ebook->Url ?>" property="schema:url"><span property="schema:name"><?= Formatter::ToPlainText($artwork->Ebook->Title) ?></span></a><? } ?></p>
 			<? if(count($artwork->ArtworkTags) > 0){ ?>
 			<p>Tags: <ul class="tags"><? foreach($artwork->ArtworkTags as $tag){ ?><li><a href="<?= $tag->Url ?>"><?= Formatter::ToPlainText($tag->Name) ?></a></li><? } ?></ul></p>
 			<? } ?>
