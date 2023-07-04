0b597cd3278005fa90f4608796c44778aa7293fe
Mike Colagrosso <colagrosso@users.noreply.github.com>
2023-07-04 01:07:00 -0600

    Style in_use artwork differently

--- templates/ArtworkList.php
+++ templates/ArtworkList.php
@@ -4,7 +4,7 @@ $artworks = $artworks ?? [];
 ?>
 <ol class="artwork-list list">
 <? foreach($artworks as $artwork){ ?>
-	<li>
+	<li class="<?= $artwork->Status ?>">
 		<div class="thumbnail-container">
 			<a href="/artworks/<?= $artwork->Slug ?>">
 				<picture>
@@ -16,6 +16,7 @@ $artworks = $artworks ?? [];
 		<p>Artist: <span class="author" typeof="schema:Person" property="schema:name"><?= Formatter::ToPlainText($artwork->Artist->Name) ?></span></p>
 		<div>
 			<p>Year completed: <? if ($artwork->CompletedYear === null){ ?>(unknown)<? }else{ ?><?= $artwork->CompletedYear ?><? if($artwork->CompletedYearIsCirca){ ?> (circa)<? } ?><? } ?></p>
+			<p>Status: <? if($artwork->Status === "approved"){ ?>Approved<? }else if($artwork->Status === "in_use"){ ?>In use by <? } ?></p>
 			<? if(count($artwork->ArtworkTags) > 0){ ?>
 			<p>Tags: <ul class="tags"><? foreach($artwork->ArtworkTags as $tag){ ?><li><a href="<?= $tag->Url ?>"><?= Formatter::ToPlainText($tag->Name) ?></a></li><? } ?></ul></p>
 			<? } ?>
