113ca4ff60978d052e92fb2abf291385f26b7237
Mike Colagrosso <colagrosso@users.noreply.github.com>
2023-07-08 22:46:15 -0600

    Add UrlName to Artists DB table

--- config/sql/se/Artists.sql
+++ config/sql/se/Artists.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `Artists` (
   `ArtistId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Name` varchar(191) NOT NULL,
+  `UrlName` varchar(255) NOT NULL,
   `DeathYear` smallint unsigned NULL,
   PRIMARY KEY (`ArtistId`),
   UNIQUE KEY `idxUnique` (`Name`,`DeathYear`)
