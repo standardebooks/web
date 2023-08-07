e8fe481032f1100b770a270b46bd4096561351ee
Szymon Szott <szott@agh.edu.pl>
2023-08-07 16:56:54 +0200

    Update wanted-ebooks.php
    Remove released book

--- www/contribute/wanted-ebooks.php
+++ www/contribute/wanted-ebooks.php
@@ -16,9 +16,6 @@
 				<p><a href="https://www.gutenberg.org/ebooks/1480">Tom Brown’s School Days</a> by Thomas Hughes</p>
 			</li>
 			<li>
-				<p><a href="https://www.gutenberg.org/ebooks/1685">The Mystery of the Yellow Room</a> by Gaston Leroux</p>
-			</li>
-			<li>
 				<p><a href="https://www.gutenberg.org/ebooks/792">Wieland</a> by Charles Brockden Brown</p>
 			</li>
 			<li>
