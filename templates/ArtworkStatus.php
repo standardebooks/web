cd7d93f32f9216cbad61834feb170dc1a6ffdb89
Mike Colagrosso <colagrosso@users.noreply.github.com>
2023-10-05 01:02:04 -0600

    Review form: Trim trailing whitespace on status

--- templates/ArtworkStatus.php
+++ templates/ArtworkStatus.php
@@ -3,14 +3,8 @@
 $artwork = $artwork ?? null;
 ?>
 <? if($artwork !== null){ ?>
-	<? if($artwork->Status === "approved"){ ?>
-		Approved
-	<? }else if($artwork->Status === "in_use"){ ?>
-		In use
-		<? if($artwork->Ebook !== null && $artwork->Ebook->Url !== null){ ?>
-			 by <a href="<?= $artwork->Ebook->Url ?>" property="schema:url"><span property="schema:name"><?= Formatter::ToPlainText($artwork->Ebook->Title) ?></span></a>
-		<? } ?>
-	<? }else{ ?>
-		<?= Formatter::ToPlainText($artwork->Status) ?>
-	<? } ?>
+<? if($artwork->Status === 'approved'){ ?>Approved<? } ?>
+<? if($artwork->Status === 'declined'){ ?>Declined<? } ?>
+<? if($artwork->Status === 'unverified'){ ?>Unverified<? } ?>
+<? if($artwork->Status === 'in_use'){ ?>In use<? if($artwork->Ebook !== null && $artwork->Ebook->Url !== null){ ?> by <a href="<?= $artwork->Ebook->Url ?>" property="schema:url"><span property="schema:name"><?= Formatter::ToPlainText($artwork->Ebook->Title) ?></span></a><? } ?><? } ?>
 <? } ?>
