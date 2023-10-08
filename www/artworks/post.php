352eb6732dd4620e7f84b9e15e13a1195cba331a
Job Curtis <job.curtis@gmail.com>
2023-10-08 21:55:24 +0100

    Set `upload_max_filesize` to 128M to allow larger image uploads. (#275)
    * Set `upload_max_filesize` to 128M to allow larger image uploads.
    
    The default is 2M, which is much too small for many images from museum
    websites.
    
    Also added feedback when a user attempts to upload an image that is too
    large.
    
    * Handle case where request is larger than `post_max_size` in `/artworks/new`
    
    * Incorporate PR feedback

--- www/artworks/post.php
+++ www/artworks/post.php
@@ -2,7 +2,20 @@
 
 require_once('Core.php');
 
-if (HttpInput::RequestMethod() != HTTP_POST){
+function post_max_size_bytes(): int{
+	$post_max_size = ini_get('post_max_size');
+	$unit = substr($post_max_size, -1);
+	$size = (int) substr($post_max_size, 0, -1);
+
+	return match ($unit) {
+		'g', 'G' => $size * 1024 * 1024 * 1024,
+		'm', 'M' => $size * 1024 * 1024,
+		'k', 'K' => $size * 1024,
+		default => $size
+	};
+}
+
+if(HttpInput::RequestMethod() != HTTP_POST){
 	http_response_code(405);
 	exit();
 }
@@ -10,6 +23,14 @@ if (HttpInput::RequestMethod() != HTTP_POST){
 session_start();
 
 try{
+	if(empty($_POST) || empty($_FILES)){
+		if($_SERVER['CONTENT_LENGTH'] > post_max_size_bytes()){
+			throw new \Exceptions\InvalidRequestException('Request too large (maximum ' . ini_get('upload_max_filesize') . ')');
+		}else{
+			throw new \Exceptions\InvalidRequestException();
+		}
+	}
+
 	$artwork = Artwork::Build(
 		artistName: HttpInput::Str(POST, 'artist-name', false),
 		artistDeathYear: HttpInput::Int(POST, 'artist-year-of-death'),
@@ -27,8 +48,19 @@ try{
 	$expectCaptcha = HttpInput::Str(SESSION, 'captcha', false);
 	$actualCaptcha = HttpInput::Str(POST, 'captcha', false);
 
-	if ($expectCaptcha === '' || mb_strtolower($expectCaptcha) !== mb_strtolower($actualCaptcha)){
+	if($expectCaptcha === '' || mb_strtolower($expectCaptcha) !== mb_strtolower($actualCaptcha)){
 		throw new Exceptions\InvalidCaptchaException();
+	}
+
+	$uploadError = $_FILES['color-upload']['error'];
+	if($uploadError > UPLOAD_ERR_OK){
+		// see https://www.php.net/manual/en/features.file-upload.errors.php
+		$message = match ($uploadError){
+			UPLOAD_ERR_INI_SIZE => 'Image upload too large (maximum ' . ini_get('upload_max_filesize') . ')',
+			default => 'Image failed to upload (error code ' . $uploadError . ')',
+		};
+
+		throw new \Exceptions\InvalidImageUploadException($message);
 	}
 
 	$artwork->Create($_FILES['color-upload']['tmp_name']);
@@ -38,7 +70,7 @@ try{
 } catch (\Exceptions\SeException $exception){
 	$_SESSION['exception'] = $exception;
 
-	if (isset($artwork)){
+	if(isset($artwork)){
 		$_SESSION['artwork'] = $artwork;
 	}
 
