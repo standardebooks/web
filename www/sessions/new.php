f06697e2b72a3c9525e51613c3767783347b9c34
Alex Cabal <alex@alexcabal.com>
2024-01-01 22:35:46 -0600

    Wording tweak on login page

--- www/sessions/new.php
+++ www/sessions/new.php
@@ -22,8 +22,8 @@ if($exception){
 ?><?= Template::Header(['title' => 'Log In', 'highlight' => '', 'description' => 'Log in to your Standard Ebooks Patrons Circle account.']) ?>
 <main>
 	<section class="narrow">
-		<h1>Log in to the Patrons Circle</h1>
-		<p>Enter your email address to log in to your Patrons Circle account. Once you’re logged in, you’ll be able to <a href="/polls">vote in our occasional polls</a>, access our <a href="/bulk-downloads">bulk ebook downloads</a>, and access our <a href="/feeds">ebook feeds</a>.</p>
+		<h1>Log in</h1>
+		<p>Enter your email address to log in to Standard Ebooks. Once you’re logged in, your Patrons Circle benefits (like <a href="/polls">voting in our occasional polls</a> and access to our <a href="/bulk-downloads">bulk ebook downloads</a> and <a href="/feeds">ebook feeds</a>) will be available to you.</p>
 		<p>Anyone can <a href="/donate#patrons-circle">join the Patrons Circle</a> with a small donation in support of our continuing mission to create free, beautiful digital literature.</p>
 		<p><strong>Important:</strong> When making your donation, you must have selected either “List my name publicly” or “Don’t list publicly, but reveal to project” on the donation form; otherwise, your email address isn’t shared with us, and we can’t include you in our login system.</p>
 		<?= Template::Error(['exception' => $exception]) ?>
