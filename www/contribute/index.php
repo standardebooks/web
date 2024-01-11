4c3922c37641d0905a7a818b5cbff0ec9f69b005
Alex Cabal <alex@alexcabal.com>
2024-01-11 11:12:24 -0600

    Add listing of research spreadsheets

--- www/contribute/index.php
+++ www/contribute/index.php
@@ -72,6 +72,9 @@
 				<li>
 					<p><a href="https://github.com/standardebooks">All Standard Ebooks ebook source files.</a></p>
 				</li>
+				<li>
+					<p><a href="/contribute/spreadsheets">All list of various research spreadsheets our volunteers created and use.</a></p>
+				</li>
 			</ul>
 		</section>
 	</article>
---
+++ www/contribute/spreadsheets.php
@@ -0,0 +1,73 @@
+<?= Template::Header(['title' => 'Research Spreadsheets', 'highlight' => 'contribute', 'description' => 'A list of spreadsheets created and used by Standard Ebooks producers.']) ?>
+<main>
+	<article>
+		<h1>Research Spreadsheets</h1>
+		<p>From time to time it’s useful to compile spreadsheets of an author’s ouevre. This page lists some of the spreadsheets our volunteers have created.</p>
+		<ul>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/18rZtciwEVfuLTJJcCh-C3u41IZ7lpmcrP8-wQoMgOMs/edit">Essays by Errico Malatesta</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1kdLJRWsGUfgpFcuEXNi05BXdrB0sKsUrgszE8DCbEIY/edit">Works by P. G. Wodehouse</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1-lzAhqdAOG9n4myr-qzu3OiJMS2wqYmMfM8xvIIF6Us/edit">Poetry by Oscar Wilde</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1O1a33gw9qJ74NWj-YMBxPJ6Vany3hhGxIRRTqjJmVLw/edit">Works by J. Sheridan Le Fanu</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1vx_nljc4SzOW6LVzgCf411jVnA6XlT6Ga8EnZFTXGoQ/edit">Plays by G. B. Shaw</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/130sDHz3rFudKxenAPi_segf-4V0yTGaeVjTyYBv8P3M/edit">Works by Algernon Blackwood</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1lDcLirkqn2eK8Pp35UqhOemozD-5lBzz-nPZ14lymJc/edit">Plays by J. M. Synge</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1lDcLirkqn2eK8Pp35UqhOemozD-5lBzz-nPZ14lymJc/edit">Works by Frederick Pohl</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/11RGuTztG5LzOfpOtchkjcuQhwkHShhE7PZL_bs4ygwg/edit">Works by H. P. Lovecraft</a> (<a href="https://docs.google.com/spreadsheets/d/11RGuTztG5LzOfpOtchkjcuQhwkHShhE7PZL_bs4ygwg/edit">Alternate spreadsheet</a>)</p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1R_fe5tNUhOmY38dnbA7n1DxJQO9KhHDwnzPgjT1ggac/edit">Works by Ambrose Bierce</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1IldpmxjK06BxPL4CCOEJZpw_bxm4AhYnrTchS03QP3k/edit">Works by Robert E. Howard</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1IldpmxjK06BxPL4CCOEJZpw_bxm4AhYnrTchS03QP3k/edit">Works by Ernest Hemingway</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1wTQTQjPDvAFDSWFyT5RY-Vw4Aop2jTs_BMIDbZXRUFs/edit">Poetry by G. K. Chesterton</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1KjbjEY3qz3pluw8cFPaKu-uUkCyb1aGuG6r4Xs_eT_E/edit">Short stories by W. S. Maugham</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1Li6i5qDxi2NZonsI7LM1C2RKPa-16-Hb8uguqyB8Lbk/edit">Works by Voltairine de Cleyre</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1xiWF5My5kCQJXrZ3NNYyUNMEMdZOQuKo-J5LGS_bVJE/edit">Poetry by Taras Shevchenko</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1Xso9v1FYk77TQgMh-NdYdO-r4WFfCwVHbDIngzNL-UM/edit">Works by Guy de Maupassant</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1yXZvlZVekVY3EpearMW8QIrbBEJlm8RbEXAuZP5rTG4/edit">Short fiction by Jack London</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1WB5Nt4LMVabt3KAUs1KxsfHfdpjU5OTs13dYITJcfLA/edit">Short fiction by Ray Bradbury</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1ArL5dfX30wa_ook1djY0AYyR191NFpv0U7IoAO2_c84/edit">Short fiction by Leo Tolstoy</a></p>
+			</li>
+			<li>
+				<p><a href="https://docs.google.com/spreadsheets/d/1vE0G9zslmxHZP17k529UiwPqSA9Zz8RBETtIHMA5Hqk/edit">Works by Thornton Burgess</a></p>
+			</li>
+		</ul>
+	</article>
+</main>
+<?= Template::Footer() ?>
