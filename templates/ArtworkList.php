7ea9f707c58dcb16f7558c17cc049cc4bf4ac244
Alex Cabal <alex@alexcabal.com>
2024-01-26 12:17:57 -0600

    Fix spacing in HTML output

--- templates/ArtworkList.php
+++ templates/ArtworkList.php
@@ -3,7 +3,7 @@ $artworks = $artworks ?? [];
 ?>
 <ol class="artwork-list">
 <? foreach($artworks as $artwork){ ?>
-	<li <? if($artwork->EbookUrl !== null){ ?> class="in-use"<? } ?>>
+	<li<? if($artwork->EbookUrl !== null){ ?> class="in-use"<? } ?>>
 		<a href="<?= $artwork->Url ?>">
 			<picture>
 				<source srcset="<?= $artwork->Thumb2xUrl ?> 2x, <?= $artwork->ThumbUrl ?> 1x" type="image/jpg"/>
