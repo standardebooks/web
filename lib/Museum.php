3f091b0aa86bcf5f15e1d88ce1842f10e2b68d49
Alex Cabal <alex@alexcabal.com>
2024-02-09 16:26:12 -0600

    Fix nationalmuseum.se URL normalization in artwork DB

--- lib/Museum.php
+++ lib/Museum.php
@@ -90,7 +90,7 @@ class Museum extends PropertiesBase{
 			return $outputUrl;
 		}
 		elseif(preg_match('/\bnationalmuseum\.se$/ius', $parsedUrl['host'])){
-			$exampleUrl = 'https://collection.nationalmuseum.se/eMP/eMuseumPlus?objectId=18217';
+			$exampleUrl = 'https://collection.nationalmuseum.se/eMP/eMuseumPlus?service=ExternalInterface&module=collection&objectId=18217';
 
 			if($parsedUrl['host'] != 'collection.nationalmuseum.se'){
 				throw new Exceptions\InvalidMuseumUrlException($url, $exampleUrl);
@@ -110,7 +110,7 @@ class Museum extends PropertiesBase{
 				throw new Exceptions\InvalidPageScanUrlException($url, $exampleUrl);
 			}
 
-			$outputUrl = 'https://' . $parsedUrl['host'] . $parsedUrl['path'] . '?objectId=' . $vars['objectId'];
+			$outputUrl = 'https://' . $parsedUrl['host'] . $parsedUrl['path'] . '?service=ExternalInterface&module=collection&objectId=' . $vars['objectId'];
 
 			return $outputUrl;
 		}
