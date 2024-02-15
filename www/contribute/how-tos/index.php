a9041234f19a4ebf043f8ba1d778125feb77b67e
chapkachapka <140322583+chapkachapka@users.noreply.github.com>
2024-02-15 10:00:34 +0000

    Add new cover image how-to

--- www/contribute/how-tos/index.php
+++ www/contribute/how-tos/index.php
@@ -7,6 +7,9 @@
 			<p><a href="/contribute/how-tos/common-issues-when-working-on-public-domain-ebooks">Common issues when working on public domain ebooks</a></p>
 		</li>
 		<li>
+			<p><a href="/contribute/how-tos/how-to-choose-and-create-a-cover-image">How to choose and create a cover image</a></p>
+		</li>
+		<li>
 			<p><a href="/contribute/how-tos/how-to-conquer-complex-drama-formatting">How to conquer complex drama formatting</a></p>
 		</li>
 		<li>
