14d6719960d0a551e448caa883aa64bbd55fc577
Alex Cabal <alex@alexcabal.com>
2024-03-07 11:04:05 -0600

    Update DB permissions for SE user

--- config/sql/users.sql
+++ config/sql/users.sql
@@ -3,7 +3,7 @@ create user 'se'@'localhost' identified by unix_socket;
 create user 'www-data'@'localhost' identified by unix_socket;
 
 grant * on * to alex@localhost identified via unix_socket;
-grant select, insert, update, delete, execute on se.* to se@localhost identified via unix_socket;
+grant select, insert, update, delete, execute, lock tables on se.* to se@localhost identified via unix_socket;
 grant select, insert, update, delete, execute on se.* to www-data@localhost identified via unix_socket;
 
 flush privileges;
