43d7b1f6e5ec4e979528601efd885cc866a0a33e
Alex Cabal <alex@alexcabal.com>
2024-03-24 14:26:57 -0500

    Add help page for canceling FA subscription

---
+++ www/help/managing-your-recurring-donation.php
@@ -0,0 +1,33 @@
+<?= Template::Header(['title' => 'Managing Your Recurring Donation', 'highlight' => 'contribute', 'manual' => false, 'description' => 'Help with managing your recurring donation to Standard Ebooks.']) ?>
+<main>
+	<article>
+		<h1>Managing Your Recurring Donation</h1>
+		<p>Our payment processor is Fractured Atlas. Unfortunately they don’t provide us a for us way to change or cancel your recurring donation on your behalf, but you can change or cancel it yourself by following these steps:</p>
+		<section id="log-in-to-your-fractured-atlas-account">
+			<h2>1. Log in to your Fractured Atlas account</h2>
+			<p>If this is your first time logging into Fractured Atlas, you’ll need to set up a new password associated with your email address. To set up a new password, visit <a href="https://fundraising.fracturedatlas.org">fundraising.fracturedatlas.org</a>, then:</p>
+		 	<ol>
+		 		<li><p>Click “sign in/up” at the upper right-hand corner of the page.</p></li>
+		 		<li><p>Then, click “Don’t Remember your Password?” and follow the prompts to set a new password.</p></li>
+		 	</ol>
+			<p><b>Note:</b> If you’re a member of Fractured Atlas and have used the same email address for your monthly donation and your membership, then you can use the login credentials for your membership to access your monthly donation dashboard.</p>
+		</section>
+		<section id="visit-your-monthly-donation-dashboard">
+			<h2>2. Visit your monthly donation dashboard</h2>
+			<p>Monthly donors have a donor dashboard where they can:</p>
+			<ul>
+				<li><p>Update payment method for each donation.</p></li>
+		    		<li><p>Update their donation amount and opt in or out of covering the fiscal sponsorship administrative fee.</p></li>
+		    		<li><p>Cancel a monthly donation.</p></li>
+		    	</ul>
+			<p><b>Note:</b> Monthly donations processed prior to February 5, 2021 and one-time donations will not appear in this dashboard.</p>
+			<p>To access this dashboard, log in to <a href="https://fundraising.fracturedatlas.org">fundraising.fracturedatlas.org</a> and click “My Donations.”</p>
+			<p>Once on your monthly donation dashboard, you can also cancel your monthly donation. To cancel your donation:</p>
+			<ol>
+				<li><p>Click the “Manage” button for the monthly donation you’d like to cancel.</p></li>
+				<li><p>Click “Looking to cancel your donation?” link near the bottom of the page.</p></li>
+			</ol>
+		</section>
+	</article>
+</main>
+<?= Template::Footer() ?>
