fd4b122e9f6f14bbc5dfe5a6270e842d09048551
Alex Cabal <alex@alexcabal.com>
2024-04-19 12:11:51 -0500

    Update wanted ebooks list

--- www/contribute/wanted-ebooks.php
+++ www/contribute/wanted-ebooks.php
@@ -159,9 +159,6 @@
 			<li>
 				<p><a href="https://www.gutenberg.org/ebooks/17957">On the Sublime</a> by Longinus</p>
 			</li>
-			<!-- <li>
-				<p><a href="https://www.gutenberg.org/ebooks/46674">Stover at Yale</a> by Owen Johnson</p>patron
-			</li> -->
 			<li>
 				<p><a href="https://www.gutenberg.org/ebooks/56426">The Mummy! A Tale of the Twenty-Second Century</a> by Jane C. Loudon</p>
 			</li>
