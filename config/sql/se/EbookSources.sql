4a1df99063296c5b31e88dda9832cc9ea074662b
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-04-21 23:10:54 -0600

    Store EbookSourceType as a string/enum, not int

--- config/sql/se/EbookSources.sql
+++ config/sql/se/EbookSources.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `EbookSources` (
   `EbookId` int(10) unsigned NOT NULL,
-  `Type` tinyint(4) unsigned NOT NULL,
+  `Type` enum('pg', 'pg_australia', 'pg_canada', 'ia', 'hathi_trust', 'wikisource', 'google_books', 'faded_page', 'other') DEFAULT 'other',
   `Url` varchar(255) NOT NULL,
   KEY `index1` (`EbookId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
