b8a5831169992f5970107f20189b7fcdfafb7a68
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-04-25 22:43:07 -0600

    Spell out EbookSourceType values

--- config/sql/se/EbookSources.sql
+++ config/sql/se/EbookSources.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `EbookSources` (
   `EbookId` int(10) unsigned NOT NULL,
-  `Type` enum('pg', 'pg_australia', 'pg_canada', 'ia', 'hathi_trust', 'wikisource', 'google_books', 'faded_page', 'other') DEFAULT 'other',
+  `Type` enum('project_gutenberg', 'project_gutenberg_australia', 'project_gutenberg_canada', 'internet_archive', 'hathi_trust', 'wikisource', 'google_books', 'faded_page', 'other') DEFAULT 'other',
   `Url` varchar(255) NOT NULL,
   KEY `index1` (`EbookId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
