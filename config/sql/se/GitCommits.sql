1e2dfc95c11b5696a71e2c97b8a2071d7067ae6e
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-05-01 00:41:22 -0600

    Add primary keys to multiple tables
    I missed these keys when creating the initial schema, and it's a useful practice.

--- config/sql/se/GitCommits.sql
+++ config/sql/se/GitCommits.sql
@@ -1,7 +1,9 @@
 CREATE TABLE `GitCommits` (
+  `GitCommitId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `EbookId` int(10) unsigned NOT NULL,
   `Created` datetime NOT NULL,
   `Message` text NOT NULL,
   `Hash` char(40) NOT NULL,
+  PRIMARY KEY (`GitCommitId`),
   KEY `index1` (`EbookId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
