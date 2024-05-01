standardebooks/web

commit 4991adb73e5e543a64f0891a30e0ef41f61fd7c6

Rebase onto master and add additional validation (#353)
* Update wanted ebooks list: Memoirs of William T. Sherman

* Update DB permissions for SE user

* Update wanted ebooks list

* Update wanted ebooks list

* Normalize non-.com Google TLDs in artwork page scans

* Add @nofollow attribute to ebook download links to prevent crawlers

* Update wanted ebooks list

* Update wanted ebooks list

* Fix MariaDB user config SQL

* Change new ebook sponsorship to per word and not levels

* Remove dead code path

* Add safe function

* Add note explaining Kindle epub 'support' to download page

* Add type hint

* Update wanted ebooks list

* Add help page for canceling FA subscription

* Add status ribbons to artworks in artworks DB

* Update wanted ebooks list

* Update wanted ebooks list

* Update wanted ebooks list

* Update business address in email footers

* Remove 'todo' tag processing

* Update wanted ebooks list

* Remove unused import

* Update wanted ebooks list

* Add X and Instagram links to site footer

* Switch from DateTime to DateTimeImmutable across codebase

* Remove obsolete info message in artwork edit form

* Remove autocomplete from new artwork form, and clarify exception for publication year page

* Remove temporary fixup tag from artworks validation

* Add icons to form elements across site, and remove some CSS classes in favor of :has()

* Sanity check to make sure 'convert' worked on TIFF file when uploading cover art

* Improve regexes for year input boxes in new artwork form

* Update wanted ebooks list

* Increase max file upload size in artwork DB

* Shorten regex to find probable british2american errors

* Add section to drama how-to

* Update wanted ebooks list

* Fix various CSS errors

* Tweak type hints in ValidationException

* Add Facebook button in footer

lib/Ebook.php
@@ -1,5 +1,5 @@
 <?
-use Safe\DateTime;
+use Safe\DateTimeImmutable;
 use function Safe\file_get_contents;
 use function Safe\filesize;
 use function Safe\json_encode;
@@ -69,8 +69,8 @@ class Ebook extends Accessor{
 	public $Contributors = [];
 	public ?string $ContributorsHtml = null;
 	public string $TitleWithCreditsHtml = '';
-	public DateTime $Created;
-	public DateTime $Updated;
+	public DateTimeImmutable $Created;
+	public DateTimeImmutable $Updated;
 	public string $TextUrl;
 	public string $TextSinglePageUrl;
 	public ?string $TextSinglePageSizeNumber = null;
@@ -265,12 +265,12 @@ class Ebook extends Accessor{
 
 		$date = $xml->xpath('/package/metadata/dc:date') ?: [];
 		if($date !== false && sizeof($date) > 0){
-			$this->Created = new DateTime((string)$date[0]);
+			$this->Created = new DateTimeImmutable((string)$date[0]);
 		}
 
 		$modifiedDate = $xml->xpath('/package/metadata/meta[@property="dcterms:modified"]') ?: [];
 		if($modifiedDate !== false && sizeof($modifiedDate) > 0){
-			$this->Updated = new DateTime((string)$modifiedDate[0]);
+			$this->Updated = new DateTimeImmutable((string)$modifiedDate[0]);
 		}
 
 		// Get SE tags
@@ -544,14 +544,85 @@ class Ebook extends Accessor{
 	// *******
 
 	public function Validate(): void{
+		$now = new DateTimeImmutable();
 		$error = new Exceptions\ValidationException();
 
 		if($this->Identifier == ''){
 			$error->Add(new Exceptions\EbookIdentifierRequiredException());
 		}
 
-		if(strlen($this->Identifier) > EBOOKS_MAX_STRING_LENGTH){
+		if(strlen($this->Identifier) > EBOOKS_MAX_LONG_STRING_LENGTH){
 			$error->Add(new Exceptions\StringTooLongException('Ebook Identifier'));
+		}
+
+		if(!is_readable($this->WwwFilesystemPath)){
+			$error->Add(new Exceptions\InvalidEbookWwwFilesystemPathException($this->WwwFilesystemPath));
+		}
+
+		if(strlen($this->WwwFilesystemPath) > EBOOKS_MAX_LONG_STRING_LENGTH){
+			$error->Add(new Exceptions\StringTooLongException('Ebook WwwFilesystemPath'));
+		}
+
+		if(!is_readable($this->RepoFilesystemPath)){
+			$error->Add(new Exceptions\InvalidEbookRepoFilesystemPathException($this->RepoFilesystemPath));
+		}
+
+		if(strlen($this->RepoFilesystemPath) > EBOOKS_MAX_LONG_STRING_LENGTH){
+			$error->Add(new Exceptions\StringTooLongException('Ebook RepoFilesystemPath'));
+		}
+
+		if($this->KindleCoverUrl !== null && !preg_match('|/*_EBOK_portrait.jpg|ius', $this->KindleCoverUrl)){
+			$error->Add(new Exceptions\InvalidEbookKindleCoverUrlException('Invalid Ebook KindleCoverUrl: ' . $this->KindleCoverUrl));
+
+		}
+
+		if($this->KindleCoverUrl !== null && strlen($this->KindleCoverUrl) > EBOOKS_MAX_LONG_STRING_LENGTH){
+			$error->Add(new Exceptions\StringTooLongException('Ebook KindleCoverUrl'));
+		}
+
+		if($this->EpubUrl !== null && !preg_match('|/*.epub|ius', $this->EpubUrl)){
+			$error->Add(new Exceptions\InvalidEbookEpubUrlException('Invalid Ebook EpubUrl: ' . $this->EpubUrl));
+
+		}
+
+		if($this->EpubUrl !== null && strlen($this->EpubUrl) > EBOOKS_MAX_LONG_STRING_LENGTH){
+			$error->Add(new Exceptions\StringTooLongException('Ebook EpubUrl'));
+		}
+
+		if($this->AdvancedEpubUrl !== null && !preg_match('|/*_advanced.epub|ius', $this->AdvancedEpubUrl)){
+			$error->Add(new Exceptions\InvalidEbookAdvancedEpubUrlException('Invalid Ebook AdvancedEpubUrl: ' . $this->AdvancedEpubUrl));
+
+		}
+
+		if($this->AdvancedEpubUrl !== null && strlen($this->AdvancedEpubUrl) > EBOOKS_MAX_LONG_STRING_LENGTH){
+			$error->Add(new Exceptions\StringTooLongException('Ebook AdvancedEpubUrl'));
+		}
+
+		if($this->KepubUrl !== null && !preg_match('|/*.kepub.epub|ius', $this->KepubUrl)){
+			$error->Add(new Exceptions\InvalidEbookKepubUrlException('Invalid Ebook KepubUrl: ' . $this->KepubUrl));
+
+		}
+
+		if($this->KepubUrl !== null && strlen($this->KepubUrl) > EBOOKS_MAX_LONG_STRING_LENGTH){
+			$error->Add(new Exceptions\StringTooLongException('Ebook KepubUrl'));
+		}
+
+		if($this->Azw3Url !== null && !preg_match('|/*.azw3|ius', $this->Azw3Url)){
+			$error->Add(new Exceptions\InvalidEbookAzw3UrlException('Invalid Ebook Azw3Url: ' . $this->Azw3Url));
+
+		}
+
+		if($this->Azw3Url !== null && strlen($this->Azw3Url) > EBOOKS_MAX_LONG_STRING_LENGTH){
+			$error->Add(new Exceptions\StringTooLongException('Ebook Azw3Url'));
+		}
+
+		if($this->DistCoverUrl !== null && !preg_match('|/*cover.jpg|ius', $this->DistCoverUrl)){
+			$error->Add(new Exceptions\InvalidEbookDistCoverUrlException('Invalid Ebook DistCoverUrl: ' . $this->DistCoverUrl));
+
+		}
+
+		if($this->DistCoverUrl !== null && strlen($this->DistCoverUrl) > EBOOKS_MAX_LONG_STRING_LENGTH){
+			$error->Add(new Exceptions\StringTooLongException('Ebook DistCoverUrl'));
 		}
 
 		if($this->Title === null || $this->Title == ''){
@@ -562,9 +633,71 @@ class Ebook extends Accessor{
 			$error->Add(new Exceptions\StringTooLongException('Ebook Title'));
 		}
 
-		// TODO: Add more validation.
+		if($this->FullTitle !== null && strlen($this->FullTitle) > EBOOKS_MAX_STRING_LENGTH){
+			$error->Add(new Exceptions\StringTooLongException('Ebook FullTitle'));
+		}
 
-		if($error->HasExceptions){
+		if($this->AlternateTitle !== null && strlen($this->AlternateTitle) > EBOOKS_MAX_STRING_LENGTH){
+			$error->Add(new Exceptions\StringTooLongException('Ebook AlternateTitle'));
+		}
+
+		if($this->Description === null || $this->Description == ''){
+			$error->Add(new Exceptions\EbookDescriptionRequiredException());
+		}
+
+		if($this->LongDescription === null || $this->LongDescription == ''){
+			$error->Add(new Exceptions\EbookLongDescriptionRequiredException());
+		}
+
+		if($this->Language !== null && strlen($this->Language) > 10){
+			$error->Add(new Exceptions\StringTooLongException('Ebook Language: ' . $this->Language));
+		}
+
+		if($this->WordCount <= 0){
+			$error->Add(new Exceptions\InvalidEbookWordCountException('Invalid Ebook WordCount: ' . $this->WordCount));
+		}
+
+		// In theory, Flesch reading ease can be negative, but in practice it's positive.
+		if($this->ReadingEase <= 0){
+			$error->Add(new Exceptions\InvalidEbookReadingEaseException('Invalid Ebook ReadingEase: ' . $this->ReadingEase));
+		}
+
+		if($this->GitHubUrl !== null && !preg_match('|https://github.com/standardebooks/\w+|ius', $this->GitHubUrl)){
+			$error->Add(new Exceptions\InvalidEbookGitHubUrlException('Invalid Ebook GitHubUrl: ' . $this->GitHubUrl));
+
+		}
+
+		if($this->GitHubUrl !== null && strlen($this->GitHubUrl) > EBOOKS_MAX_STRING_LENGTH){
+			$error->Add(new Exceptions\StringTooLongException('Ebook GitHubUrl'));
+		}
+
+		if($this->WikipediaUrl !== null && !preg_match('|https://.*wiki.*|ius', $this->WikipediaUrl)){
+			$error->Add(new Exceptions\InvalidEbookWikipediaUrlException('Invalid Ebook WikipediaUrl: ' . $this->WikipediaUrl));
+
+		}
+
+		if($this->WikipediaUrl !== null && strlen($this->WikipediaUrl) > EBOOKS_MAX_STRING_LENGTH){
+			$error->Add(new Exceptions\StringTooLongException('Ebook WikipediaUrl'));
+		}
+
+		if($this->Created > $now || $this->Created < EBOOK_EARLIEST_CREATION_DATE){
+			$error->Add(new Exceptions\InvalidEbookCreatedDatetimeException($this->Created));
+		}
+
+		if($this->Updated > $now || $this->Updated < EBOOK_EARLIEST_CREATION_DATE){
+			$error->Add(new Exceptions\InvalidEbookUpdatedDatetimeException($this->Updated));
+
+		}
+
+		if($this->TextSinglePageByteCount === null || $this->TextSinglePageByteCount <= 0){
+			$error->Add(new Exceptions\InvalidEbookTextSinglePageByteCountException('Invalid Ebook TextSinglePageByteCount: ' . $this->TextSinglePageByteCount));
+		}
+
+		if($this->IndexableText === null || $this->IndexableText == ''){
+			$error->Add(new Exceptions\EbookIndexableTextRequiredException());
+		}
+
+    if($error->HasExceptions){
 			throw $error;
 		}
 	}

commit b2519cc67c4ef6e7e6599b4cdc05bee95cbaca80

Fix tab/space error introduced in the #353 merge
https://github.com/standardebooks/web/pull/353/commits/6ae5cda1d98142537eca1fbe7e00021118128c7f

lib/Ebook.php
@@ -697,7 +697,7 @@ class Ebook extends Accessor{
 			$error->Add(new Exceptions\EbookIndexableTextRequiredException());
 		}
 
-    if($error->HasExceptions){
+	if($error->HasExceptions){
 			throw $error;
 		}
 	}