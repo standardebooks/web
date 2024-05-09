d1d0475c21a1a38ea7fce69dd135e2a42760ba4f
Lukas Bystricky <lb13f@my.fsu.edu>
2024-05-09 12:05:28 +0200

    Move proofreading guidelines to separate page

--- www/contribute/producing-an-ebook-step-by-step.php
+++ www/contribute/producing-an-ebook-step-by-step.php
@@ -591,25 +591,7 @@ proceed to seal up my confession, I bring the life of that unhappy Henry Jekyll 
 				</ul>
 				<p>Now, transfer the ebook to your ereader and start a cover-to-cover proofread.</p>
 				<h3>What do we mean by “proofreading”?</h3>
-				<p>“Proofreading” means a close reading of the text to try to spot any transcription errors or issues which the <abbr class="acronym">SEMoS</abbr> says we must update. It’s typically <em>not</em> a line-by-line comparison to the page scans—that work was already done by the initial transcriber. Rather, proofreading is reading the book as you would any other book, but with careful attention to possible problems in the transcription or in your production.</p>
-				<h3>Common proofreading issues</h3>
-				<ul>
-					<li><p><b>Missing or incorrect punctuation.</b> Often <abbr class="initialism">O.C.R.</abbr> software misses small punctuation marks like commas and periods. Does a sentence sound awkward, as if it was missing a comma? Is a period obviously missing between sentences? Mark it and check it against the page scans.</p></li>
-					<li><p><b>Missing formatting.</b> Transcribers often remove formatting like blockquotes or italics. Is there a section in the book that looks like it should be styled as a blockquote, like verse or a letter? Are characters speaking emphatically, but without italics? Mark these cases to compare against the page scans to see if formatting has to be restored.</p></li>
-					<li><p><b>Missing thought or paragraph breaks.</b> Is a paragraph unusually long? Does a scene change occur without <code class="html"><span class="p">&lt;</span><span class="nt">hr</span>/<span class="p">&gt;</span></code>? They might have been lost during transcription.</p></li>
-					<li><p><b>Errors caused by the S.E. toolset.</b> Tools like <code class="bash"><b>se</b> british2american</code> or even <code class="bash"><b>se</b> typogrify</code> can cause unexpected typography errors like quotation marks curled in the wrong direction, or dashes spaced incorrectly.</p></li>
-					<li><p><b>Archaic spellings.</b> Is a particular word spelled in a surprising way? Mark it to check if it should be modernized.  The <a href="https://books.google.com/ngrams/">Google Books Ngram Viewer</a> is a great tool to get an idea of whether a word used to be spelled one way, but isn’t spelled that way anymore. Remember to change spellings in their own commits, prefaced with <code class="html">[Editorial]</code>!</p></li>
-					<li><p><b>Printer’s errors.</b> Before the computer age, hand-written manuscripts were sent to printers, whose job it was to lay the book out in lead blocks of type. During this process, the printer may have accidentally introduced their own typos, which are usually obvious errors in punctuation, spelling, or grammar. If you spot an obvious error like this that is present in both the transcription and the page scans, it may be a printer’s error that should be corrected in an <code class="html">[Editorial]</code> commit.</p></li>
-				</ul>
-				<p>There are some things that you don’t have to worry much about when proofreading:</p>
-				<ul>
-					<li><p><b>Spelling errors.</b> Actual spelling errors are very rare. If a word appears to be misspelled, it’s worth it to check the page scans, but such cases are often done on purpose by the author, or using an older spelling, or are spelled differently in en-US vs. en-GB.</p></li>
-					<li><p><b>Changing from en-GB to en-US or vice versa.</b> Spelling differences between the continents were not yet settled then, so it’s common for books to be set in a blend of spellings. We don’t convert from one style to the other, or try to ensure perfect consistency between styles. This includes en-GB differences like using “an” in front of a word starting in “h,” like “an hundred.”</p></li>
-					<li><p><b>Keeping a 100% faithful representation of a print page layout.</b> Sometimes books have complicated page layouts in print. But ebooks are not the same as print books, with the most important distinction being that there is no “page” to align items to. So, we’re not so concerned with maintaining a pixel-perfect reproduction of print layouts; rather, we wish to <em>adapt</em> print layouts as best we can to the ebook medium.</p></li>
-				</ul>
-				<aside class="tip">
-					<p>If you’re using a transcription from Project Gutenberg as the base for this ebook, you may wish to report typos you’ve found to them, so that they can correct their copy. <a href="/contribute/report-errors-upstream">Instructions for how to do so are here.</a></p>
-				</aside>
+				<p>“Proofreading” means a close reading of the text to try to spot any transcription errors or issues which the <abbr class="acronym">SEMoS</abbr> says we must update. It’s typically <em>not</em> a line-by-line comparison to the page scans—that work was already done by the initial transcriber. Rather, proofreading is reading the book as you would any other book, but with careful attention to possible problems in the transcription or in your production. For some general tips on what to look out for when proofreading see the guide <a href="/contribute/how-tos/things-to-look-out-for-when-proofreading">here</a>.</p>
 			</li>
 			<li>
 				<h2 id="cover">Create the cover image</h2>
