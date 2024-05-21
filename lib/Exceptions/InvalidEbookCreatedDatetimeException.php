406eff56a904b8d3b5fd1ad6a55fae860e5e1330
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-05-20 20:37:25 -0600

    Fix PHPStan errors

--- lib/Exceptions/InvalidEbookCreatedDatetimeException.php
+++ lib/Exceptions/InvalidEbookCreatedDatetimeException.php
@@ -4,8 +4,12 @@ namespace Exceptions;
 use Safe\DateTimeImmutable;
 
 class InvalidEbookCreatedDatetimeException extends AppException{
+	/** @var string $message */
 	protected $message = 'Invalid EbookCreated datetime.';
 
+	/**
+	 * @throws \Exception
+	 */
 	public function __construct(DateTimeImmutable $createdDatetime){
 		$now = new DateTimeImmutable();
 		$this->message = 'Invalid EbookCreated datetime. ' . $createdDatetime->format('Y-m-d') . ' is not between ' . EBOOK_EARLIEST_CREATION_DATE->format('Y-m-d') . ' and ' . $now->format('Y-m-d') . '.';
