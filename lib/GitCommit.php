814cdb796ea3c7c645d443b2f35ae6c573d5aa94
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-05-20 22:33:16 -0600

    Rename the constructor `Ebook::__construct()` to `static Ebook::FromFilesystem()`
    Also added `GetFoo()` methods for all the derived properties like `GetUrl()`, `GetHasDownloads()`, etc. Removed that logic from the constructor so that it's reusable in `Ebook::FromFilesystem()` and `Ebook::GetByIdentifier()`

--- lib/GitCommit.php
+++ lib/GitCommit.php
@@ -9,14 +9,16 @@ class GitCommit{
 	/**
 	 * @throws Exceptions\InvalidGitCommitException
 	 */
-	public function __construct(string $unixTimestamp, string $hash, string $message){
+	public static function FromLog(string $unixTimestamp, string $hash, string $message): GitCommit{
+		$instance = new GitCommit();
 		try{
-			$this->Created = new DateTimeImmutable('@' . $unixTimestamp);
+			$instance->Created = new DateTimeImmutable('@' . $unixTimestamp);
 		}
 		catch(\Exception){
 			throw new Exceptions\InvalidGitCommitException('Invalid timestamp for Git commit.');
 		}
-		$this->Message = $message;
-		$this->Hash = $hash;
+		$instance->Message = $message;
+		$instance->Hash = $hash;
+		return $instance;
 	}
 }
