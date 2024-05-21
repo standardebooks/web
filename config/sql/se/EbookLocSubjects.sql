ac7db8d375a5fa2b662bb3b266c4c9581a44aa26
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-05-20 22:12:36 -0600

    Add primary keys to LocSubjects and Tags tables
    Missed these before, and in addition to being a good practice, they are needed to maintain insertion order of LocSubjects and Tags.

--- config/sql/se/EbookLocSubjects.sql
+++ config/sql/se/EbookLocSubjects.sql
@@ -1,5 +1,7 @@
 CREATE TABLE `EbookLocSubjects` (
+  `EbookLocSubjectId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `EbookId` int(10) unsigned NOT NULL,
   `LocSubjectId` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`EbookLocSubjectId`),
   UNIQUE KEY `idxUnique` (`EbookId`,`LocSubjectId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
