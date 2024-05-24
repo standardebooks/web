76fa58de02171dc38655d667c00d9eb1b2875217
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-05-23 22:13:45 -0600

    Workaround a bug in Safe-PHP causes PHPStan errors

--- lib/Exceptions/InvalidEbookUpdatedDatetimeException.php
+++ lib/Exceptions/InvalidEbookUpdatedDatetimeException.php
@@ -7,10 +7,8 @@ class InvalidEbookUpdatedDatetimeException extends AppException{
 	/** @var string $message */
 	protected $message = 'Invalid EbookUpdated datetime.';
 
-	/**
-	 * @throws \Exception
-	 */
 	public function __construct(DateTimeImmutable $updatedDatetime){
+		/** @throws void */
 		$now = new DateTimeImmutable();
 		$this->message = 'Invalid EbookUpdated datetime. ' . $updatedDatetime->format('Y-m-d') . ' is not between ' . EBOOK_EARLIEST_CREATION_DATE->format('Y-m-d') . ' and ' . $now->format('Y-m-d') . '.';
 	}
