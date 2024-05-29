d39076d85818e6093594a73ddb3006aa0fdc5db9
Robin Whittleton <robin@reala.net>
2024-05-29 20:44:44 +0200

    Speed up reading of “More books” links
    The links are fine on their own, don’t need to be announced as the covers.

--- www/ebooks/ebook.php
+++ www/ebooks/ebook.php
@@ -405,7 +405,7 @@ catch(Exceptions\EbookNotFoundException){
 						<picture>
 							<? if($carouselEbook->CoverImage2xAvifUrl !== null){ ?><source srcset="<?= $carouselEbook->CoverImage2xAvifUrl ?> 2x, <?= $carouselEbook->CoverImageAvifUrl ?> 1x" type="image/avif"/><? } ?>
 							<source srcset="<?= $carouselEbook->CoverImage2xUrl ?> 2x, <?= $carouselEbook->CoverImageUrl ?> 1x" type="image/jpg"/>
-							<img src="<?= $carouselEbook->CoverImageUrl ?>" alt="The cover for the Standard Ebooks edition of <?= Formatter::EscapeHtml(strip_tags($carouselEbook->TitleWithCreditsHtml)) ?>" height="200" width="134" loading="lazy"/>
+							<img src="<?= $carouselEbook->CoverImageUrl ?>" alt="<?= Formatter::EscapeHtml(strip_tags($carouselEbook->TitleWithCreditsHtml)) ?>" height="200" width="134" loading="lazy"/>
 						</picture>
 					</a>
 				</li>
