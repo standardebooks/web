colagrosso/se-web

Rename the constructor `Ebook::__construct()` to `static Ebook::FromFilesystem()`
Also added `GetFoo()` methods for all the derived properties like `GetUrl()`, `GetHasDownloads()`, etc. Removed that logic from the constructor so that it's reusable in `Ebook::FromFilesystem()` and `Ebook::GetByIdentifier()`

lib/Collection.php
@@ -1,17 +1,31 @@
 <?
 use function Safe\preg_replace;
 
+/**
+ * @property string $Url
+ */
 class Collection{
+	use Traits\Accessor;
+
 	public string $Name;
 	public string $UrlName;
-	public string $Url;
 	public ?int $SequenceNumber = null;
 	public ?string $Type = null;
+	protected ?string $_Url = null;
 
-	public function __construct(string $name){
-		$this->Name = $name;
-		$this->UrlName = Formatter::MakeUrlSafe($this->Name);
-		$this->Url = '/collections/' . $this->UrlName;
+	protected function GetUrl(): ?string{
+		if($this->_Url === null){
+			$this->Url = '/collections/' . $this->UrlName;
+		}
+
+		return $this->_Url;
+	}
+
+	public static function FromFile(string $name): Collection{
+		$instance = new Collection();
+		$instance->Name = $name;
+		$instance->UrlName = Formatter::MakeUrlSafe($instance->Name);
+		return $instance;
 	}
 
 	public function GetSortedName(): string{

lib/Contributor.php
@@ -8,13 +8,15 @@ class Contributor{
 	public ?string $FullName = null;
 	public ?string $NacoafUrl = null;
 
-	public function __construct(string $name, string $sortName = null, string $fullName = null, string $wikipediaUrl = null, string $marcRole = null, string $nacoafUrl = null){
-		$this->Name = str_replace('\'', '’', $name);
-		$this->UrlName = Formatter::MakeUrlSafe($name);
-		$this->SortName = $sortName;
-		$this->FullName = $fullName;
-		$this->WikipediaUrl = $wikipediaUrl;
-		$this->MarcRole = $marcRole;
-		$this->NacoafUrl = $nacoafUrl;
+	public static function FromFile(string $name, string $sortName = null, string $fullName = null, string $wikipediaUrl = null, string $marcRole = null, string $nacoafUrl = null): Contributor{
+		$instance = new Contributor();
+		$instance->Name = str_replace('\'', '’', $name);
+		$instance->UrlName = Formatter::MakeUrlSafe($name);
+		$instance->SortName = $sortName;
+		$instance->FullName = $fullName;
+		$instance->WikipediaUrl = $wikipediaUrl;
+		$instance->MarcRole = $marcRole;
+		$instance->NacoafUrl = $nacoafUrl;
+		return $instance;
 	}
 }

lib/EbookSource.php
@@ -3,8 +3,10 @@ class EbookSource{
 	public EbookSourceType $Type;
 	public string $Url;
 
-	public function __construct(EbookSourceType $type, string $url){
-		$this->Type = $type;
-		$this->Url = $url;
+	public static function FromFile(EbookSourceType $type, string $url): EbookSource{
+		$instance = new EbookSource();
+		$instance->Type = $type;
+		$instance->Url = $url;
+		return $instance;
 	}
 }

standardebooks/web

lib/Collection.php
@@ -1,17 +1,31 @@
 <?
 use function Safe\preg_replace;
 
+/**
+ * @property string $Url
+ */
 class Collection{
+	use Traits\Accessor;
+
 	public string $Name;
 	public string $UrlName;
-	public string $Url;
 	public ?int $SequenceNumber = null;
 	public ?string $Type = null;
+	protected ?string $_Url = null;
 
-	public function __construct(string $name){
-		$this->Name = $name;
-		$this->UrlName = Formatter::MakeUrlSafe($this->Name);
-		$this->Url = '/collections/' . $this->UrlName;
+	protected function GetUrl(): ?string{
+		if($this->_Url === null){
+			$this->Url = '/collections/' . $this->UrlName;
+		}
+
+		return $this->_Url;
+	}
+
+	public static function FromName(string $name): Collection{
+		$instance = new Collection();
+		$instance->Name = $name;
+		$instance->UrlName = Formatter::MakeUrlSafe($instance->Name);
+		return $instance;
 	}
 
 	public function GetSortedName(): string{

lib/Contributor.php
@@ -8,13 +8,15 @@ class Contributor{
 	public ?string $FullName = null;
 	public ?string $NacoafUrl = null;
 
-	public function __construct(string $name, string $sortName = null, string $fullName = null, string $wikipediaUrl = null, string $marcRole = null, string $nacoafUrl = null){
-		$this->Name = str_replace('\'', '’', $name);
-		$this->UrlName = Formatter::MakeUrlSafe($name);
-		$this->SortName = $sortName;
-		$this->FullName = $fullName;
-		$this->WikipediaUrl = $wikipediaUrl;
-		$this->MarcRole = $marcRole;
-		$this->NacoafUrl = $nacoafUrl;
+	public static function FromProperties(string $name, string $sortName = null, string $fullName = null, string $wikipediaUrl = null, string $marcRole = null, string $nacoafUrl = null): Contributor{
+		$instance = new Contributor();
+		$instance->Name = str_replace('\'', '’', $name);
+		$instance->UrlName = Formatter::MakeUrlSafe($name);
+		$instance->SortName = $sortName;
+		$instance->FullName = $fullName;
+		$instance->WikipediaUrl = $wikipediaUrl;
+		$instance->MarcRole = $marcRole;
+		$instance->NacoafUrl = $nacoafUrl;
+		return $instance;
 	}
 }

lib/EbookSource.php
@@ -3,8 +3,10 @@ class EbookSource{
 	public EbookSourceType $Type;
 	public string $Url;
 
-	public function __construct(EbookSourceType $type, string $url){
-		$this->Type = $type;
-		$this->Url = $url;
+	public static function FromTypeAndUrl(EbookSourceType $type, string $url): EbookSource{
+		$instance = new EbookSource();
+		$instance->Type = $type;
+		$instance->Url = $url;
+		return $instance;
 	}
 }