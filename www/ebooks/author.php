104bcdca6207502a6901ccb702ba212fe0e35b4c
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-06-30 21:50:52 -0600

    Query Contributors table for GetEbooksByAuthor()

--- www/ebooks/author.php
+++ www/ebooks/author.php
@@ -12,7 +12,7 @@ try{
 		throw new Exceptions\AuthorNotFoundException();
 	}
 
-	$ebooks = Library::GetEbooksByAuthor($wwwFilesystemPath);
+	$ebooks = Library::GetEbooksByAuthor($urlPath);
 
 	if(sizeof($ebooks) == 0){
 		throw new Exceptions\AuthorNotFoundException();
