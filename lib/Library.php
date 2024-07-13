d8934676681165a248c5af547f7d056bf16ce1a7
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-07-13 15:33:42 -0600

    Rename GetAllArtists to GetArtists

--- lib/Library.php
+++ lib/Library.php
@@ -808,7 +808,7 @@ class Library{
 	/**
 	 * @return array<Artist>
 	 */
-	public static function GetAllArtists(): array{
+	public static function GetArtists(): array{
 		return Db::Query('
 			SELECT *
 			from Artists
