580ceeada80da26aa564e26b7dc9d7ddfbf17651
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-09-09 22:34:36 -0600

    Ebook tags: Store UrlName so it can be queried

--- config/sql/se/Tags.sql
+++ config/sql/se/Tags.sql
@@ -1,8 +1,10 @@
 CREATE TABLE `Tags` (
   `TagId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) NOT NULL,
+  `UrlName` varchar(255) NULL,
   `Type` enum('artwork', 'ebook') DEFAULT 'artwork',
   PRIMARY KEY (`TagId`),
   KEY `index1` (`Name`),
-  KEY `index2` (`Type`)
+  KEY `index2` (`Type`),
+  KEY `index3` (`UrlName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
