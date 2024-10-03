8c90c41d144b74347c180817147efcf51d342e98
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-10-03 14:05:53 -0600

    Collection validation: Pass enum to InvalidCollectionTypeException, not value

--- lib/Exceptions/InvalidCollectionTypeException.php
+++ lib/Exceptions/InvalidCollectionTypeException.php
@@ -2,13 +2,15 @@
 
 namespace Exceptions;
 
+use \CollectionType;
+
 class InvalidCollectionTypeException extends AppException{
 	/** @var string $message */
 	protected $message = 'Collection type should be `series` or `set` according to the EPUB specification.';
 
-	public function __construct(?string $collectionType){
-		if($collectionType !== null && trim($collectionType) != ''){
-			$this->message .= ' Type provided: ' . $collectionType;
+	public function __construct(?CollectionType $collectionType){
+		if($collectionType !== null){
+			$this->message .= ' Type provided: ' . $collectionType->value;
 		}
 
 		parent::__construct($this->message);
