b6fbcaba263b5aa62e06aa30f8ff466a6e81ece2
Alex Cabal <alex@alexcabal.com>
2024-10-22 22:47:56 -0500

    Remove irrelevant user from SQL config

--- config/sql/users.sql
+++ config/sql/users.sql
@@ -1,9 +1,7 @@
-create user 'alex'@'localhost' identified via unix_socket;
 create user 'se'@'localhost' identified via unix_socket;
 create user 'www-data'@'localhost' identified via unix_socket;
 create user 'se-vcs-bot'@'localhost' identified via unix_socket;
 
-grant * on * to alex@localhost identified via unix_socket;
 grant select, insert, update, delete, execute, lock tables on se.* to 'se'@'localhost' identified via unix_socket;
 grant select, insert, update, delete, execute on se.* to 'www-data'@'localhost' identified via unix_socket;
 grant select, insert, update, delete, execute on se.* to 'se-vcs-bot'@'localhost' identified via unix_socket;
