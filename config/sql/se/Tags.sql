b3a9d8682856af587f75e282b289071152c46038
Alex Cabal <alex@alexcabal.com>
2024-11-11 11:21:21 -0600

    Fix SQL definition

--- config/sql/se/Tags.sql
+++ config/sql/se/Tags.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `Tags` (
   `TagId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) NOT NULL,
-  `UrlName` varchar(255) NULL,
+  `UrlName` varchar(255) NOT NULL,
   `Type` enum('artwork', 'ebook') DEFAULT 'artwork',
   PRIMARY KEY (`TagId`),
   KEY `index1` (`Name`),
