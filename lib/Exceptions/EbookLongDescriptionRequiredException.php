10edd2a7298a7a32228ecb769634b4e841b51852
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-11-21 22:47:17 -0700

    Update several `Ebook` fields to nullable
    `EbookPlaceholder`s won't have `WwwFilesystemPath`, `RepoFilesystemPath`, etc.,
    so they must be nullable in the DB.
    
    The remaining `NOT NULL` fields are:
    
    * `EbookId`
    * `Identifier`
    * `Created`
    * `Updated`
    * `Title`
    * `IndexableText`

--- lib/Exceptions/EbookLongDescriptionRequiredException.php
+++
@@ -1,7 +0,0 @@
-<?
-namespace Exceptions;
-
-class EbookLongDescriptionRequiredException extends AppException{
-	/** @var string $message */
-	protected $message = 'Ebook LongDescription required.';
-}
