8692e7a7d2fd226fc7ecf3e4e233c9bd5d0c1176
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-12-09 10:35:29 -0700

    Show "unreleased" on placeholder ebooks
    The ebook link will work to the placeholder, where it will be obvious that the
    ebook is a placeholder, but it's useful to see that on the artwork page, too.

--- templates/ArtworkStatus.php
+++ templates/ArtworkStatus.php
@@ -14,7 +14,7 @@
 			<? if($artwork->Ebook !== null && $artwork->Ebook->Url !== null){ ?>
 				<i>
 					<a href="<?= $artwork->Ebook->Url ?>"><?= Formatter::EscapeHtml($artwork->Ebook->Title) ?></a>
-				</i>
+				</i><? if($artwork->Ebook->IsPlaceholder()){ ?>(unreleased)<? } ?>
 			<? }else{ ?>
 				<code><?= Formatter::EscapeHtml($artwork->EbookUrl) ?></code> (unreleased)
 			<? } ?>
