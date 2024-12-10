b40d90e540d2b6381f6c7b7c3185394d51fe3a23
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-12-10 12:56:39 -0700

    Allow Status to be null
    It applies only when IsWanted is true, so we must allow nulls for the times
    IsWanted is false.

--- config/sql/se/EbookPlaceholders.sql
+++ config/sql/se/EbookPlaceholders.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `EbookPlaceholders` (
   `EbookId` int(10) unsigned NOT NULL,
   `YearPublished` smallint unsigned NULL,
-  `Status` enum('wanted', 'in_progress') NOT NULL DEFAULT 'wanted',
+  `Status` enum('wanted', 'in_progress') NULL,
   `Difficulty` enum('beginner', 'intermediate', 'advanced') NULL,
   `TranscriptionUrl` varchar(511) NULL,
   `IsWanted` boolean NOT NULL DEFAULT FALSE,
