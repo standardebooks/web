3bdc8b0aeaa33d934b8563d0d8dbdceec448efd0
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-12-24 00:39:41 -0700

    Add Template::CollectionFormatted for common formatting

---
+++ templates/CollectionFormatted.php
@@ -0,0 +1,10 @@
+<?
+use function Safe\preg_replace;
+
+$collectionMembership = $collectionMembership ?? null;
+$collection = $collectionMembership?->Collection;
+$sequenceNumber = $collectionMembership?->SequenceNumber;
+?>
+<? if($sequenceNumber !== null){ ?>№ <?= number_format($sequenceNumber) ?> in the<? }else{ ?>Part of the<? } ?> <a href="<?= $collection->Url ?>" property="schema:isPartOf"><?= Formatter::EscapeHtml(preg_replace('/^The /ius', '', (string)$collection->Name)) ?></a>
+<? if($collection->Type !== null){ ?>
+	<? if(substr_compare(mb_strtolower($collection->Name), mb_strtolower($collection->Type->value), -strlen(mb_strtolower($collection->Type->value))) !== 0){ ?><?= $collection->Type->value ?><? } ?><? }else{ ?>collection<? } ?>
