5c8315e6ff46c70ca148ddb4a6210281a3c17b68
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-12-31 13:04:50 -0700

    Add new indices for deleting unused records

--- config/sql/se/EbookLocSubjects.sql
+++ config/sql/se/EbookLocSubjects.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS `EbookLocSubjects` (
   `EbookId` int(10) unsigned NOT NULL,
   `LocSubjectId` int(10) unsigned NOT NULL,
   `SortOrder` tinyint(3) unsigned NOT NULL,
-  UNIQUE KEY `idxUnique` (`EbookId`,`LocSubjectId`)
+  UNIQUE KEY `idxUnique` (`EbookId`,`LocSubjectId`),
+  KEY `index1` (`LocSubjectId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- config/sql/se/EbookTags.sql
+++ config/sql/se/EbookTags.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS `EbookTags` (
   `EbookId` int(10) unsigned NOT NULL,
   `TagId` int(10) unsigned NOT NULL,
   `SortOrder` tinyint(3) unsigned NOT NULL,
-  UNIQUE KEY `idxUnique` (`EbookId`,`TagId`)
+  UNIQUE KEY `idxUnique` (`EbookId`,`TagId`),
+  KEY `index1` (`TagId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
