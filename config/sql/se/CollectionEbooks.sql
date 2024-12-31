aef02f1c88e879aa7bf90e13235e466cc6dddf71
Mike Colagrosso <colagrosso@users.noreply.github.com>
2024-12-31 13:04:50 -0700

    Add new indices for deleting unused records

--- config/sql/se/CollectionEbooks.sql
+++ config/sql/se/CollectionEbooks.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS `CollectionEbooks` (
   `CollectionId` int(10) unsigned NOT NULL,
   `SequenceNumber` int(10) unsigned NULL,
   `SortOrder` tinyint(3) unsigned NOT NULL,
-  UNIQUE KEY `idxUnique` (`EbookId`,`CollectionId`)
+  UNIQUE KEY `idxUnique` (`EbookId`,`CollectionId`),
+  KEY `index1` (`CollectionId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
