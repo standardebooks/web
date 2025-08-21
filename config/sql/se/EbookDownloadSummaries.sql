81a6af6bdb9fe5edf67bf340468795a5e9d6e66a
Mike Colagrosso <colagrosso@users.noreply.github.com>
2025-08-20 22:47:10 -0600

    Track unique non-bot downloads in `EbookDownloadSummaries`
    This requires a schema change to existing DBs:
    
    ```
    ALTER TABLE `EbookDownloadSummaries`
    ADD COLUMN `UniqueDownloadCount` int(10) unsigned NOT NULL DEFAULT 0
    AFTER `BotDownloadCount`;
    ```

--- config/sql/se/EbookDownloadSummaries.sql
+++ config/sql/se/EbookDownloadSummaries.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `EbookDownloadSummaries` (
   `Date` date NOT NULL,
   `DownloadCount` int(10) unsigned NOT NULL DEFAULT 0,
   `BotDownloadCount` int(10) unsigned NOT NULL DEFAULT 0,
+  `UniqueDownloadCount` int(10) unsigned NOT NULL DEFAULT 0,
   UNIQUE INDEX `idxUnique` (`EbookId`, `Date`),
   INDEX `index1` (Date, EbookId, DownloadCount),
   INDEX `index2` (EbookId, DownloadCount)
