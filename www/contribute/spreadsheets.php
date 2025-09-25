727c330a45a002e8c0e253f086181949ad658218
Alex Cabal <alex@alexcabal.com>
2025-09-25 14:44:54 -0500

    Add research spreadsheet template and instructions

--- www/contribute/spreadsheets.php
+++ www/contribute/spreadsheets.php
@@ -3,164 +3,189 @@
 	<article>
 		<h1>Research Spreadsheets</h1>
 		<p>From time to time it’s useful to compile spreadsheets of an author’s oeuvre. This page lists some of the spreadsheets our volunteers have created.</p>
-		<ul>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1hqz9yCVfPkw3IrOhkQASaPGy2oaTgNWKH1vmZd50384/edit#gid=0">Poetry by Langston Hughes</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/18rZtciwEVfuLTJJcCh-C3u41IZ7lpmcrP8-wQoMgOMs/edit">Essays by Errico Malatesta</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1kdLJRWsGUfgpFcuEXNi05BXdrB0sKsUrgszE8DCbEIY/edit">Works by P. G. Wodehouse</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1-lzAhqdAOG9n4myr-qzu3OiJMS2wqYmMfM8xvIIF6Us/edit">Poetry by Oscar Wilde</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1O1a33gw9qJ74NWj-YMBxPJ6Vany3hhGxIRRTqjJmVLw/edit">Works by J. Sheridan Le Fanu</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1vx_nljc4SzOW6LVzgCf411jVnA6XlT6Ga8EnZFTXGoQ/edit">Plays by G. B. Shaw</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/130sDHz3rFudKxenAPi_segf-4V0yTGaeVjTyYBv8P3M/edit">Works by Algernon Blackwood</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1lDcLirkqn2eK8Pp35UqhOemozD-5lBzz-nPZ14lymJc/edit">Plays by J. M. Synge</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1C-v7kjwZ0rbkp0C2pjInTh35d6jKA-BEoKvS500Agz8/edit">Works by Frederick Pohl</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/11RGuTztG5LzOfpOtchkjcuQhwkHShhE7PZL_bs4ygwg/edit">Works by H. P. Lovecraft</a> (<a href="https://docs.google.com/spreadsheets/d/11RGuTztG5LzOfpOtchkjcuQhwkHShhE7PZL_bs4ygwg/edit">Alternate spreadsheet</a>)</p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1R_fe5tNUhOmY38dnbA7n1DxJQO9KhHDwnzPgjT1ggac/edit">Works by Ambrose Bierce</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1IldpmxjK06BxPL4CCOEJZpw_bxm4AhYnrTchS03QP3k/edit">Works by Robert E. Howard</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1IldpmxjK06BxPL4CCOEJZpw_bxm4AhYnrTchS03QP3k/edit">Works by Ernest Hemingway</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1wTQTQjPDvAFDSWFyT5RY-Vw4Aop2jTs_BMIDbZXRUFs/edit">Poetry by G. K. Chesterton</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1KjbjEY3qz3pluw8cFPaKu-uUkCyb1aGuG6r4Xs_eT_E/edit">Short stories by W. S. Maugham</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1Li6i5qDxi2NZonsI7LM1C2RKPa-16-Hb8uguqyB8Lbk/edit">Works by Voltairine de Cleyre</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1xiWF5My5kCQJXrZ3NNYyUNMEMdZOQuKo-J5LGS_bVJE/edit">Poetry by Taras Shevchenko</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1Xso9v1FYk77TQgMh-NdYdO-r4WFfCwVHbDIngzNL-UM/edit">Works by Guy de Maupassant</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1yXZvlZVekVY3EpearMW8QIrbBEJlm8RbEXAuZP5rTG4/edit">Short fiction by Jack London</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1WB5Nt4LMVabt3KAUs1KxsfHfdpjU5OTs13dYITJcfLA/edit">Short fiction by Ray Bradbury</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1ArL5dfX30wa_ook1djY0AYyR191NFpv0U7IoAO2_c84/edit">Short fiction by Leo Tolstoy</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1vE0G9zslmxHZP17k529UiwPqSA9Zz8RBETtIHMA5Hqk/edit">Works by Thornton Burgess</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1rXHudjoiDoaWsS48KEw4lJWBVFVKkmhjqzaOKVB0IWQ/edit">Short fiction by Hans Christian Anderson</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1WkpEyYxPD21PbXH9yS5VZW1UcbhBNSnIFKJIS2wgA8A/edit">Short fiction by Rudyard Kipling</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1I4kXf3z2pmHTIVt7V3K57XiFmwof21o4uQOMNPK2oLo/edit">Short fiction by Sabine Baring-Gould</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1nLP-Ij0RTM15X8Xx6o_vV45GVdE2EiylCI2SRT2rN1Q/edit">Short fiction by E. F. Benson</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1ydCo8uwS4dJfSJoFgqIIAy9AuSfNcq0UhmKQ447-wY0/edit">Short fiction by Frank Belknap</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1jGIMLt2YaDT80312vNUfZBPbYxJBXHSCOYYtE85ILuk/edit">Short fiction by Nella Larsen</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1oYtEyX5ymsm8EN45LORUoh_WOKmiigu3GvMwSMzdizU/edit">Short fiction by Nelson S. Bond</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/17s4F5KlwHUODq87DUzEXWA0yAt7s2aeHBV52iDWpAsI/edit">Short fiction by F. Scott Fitzgerald</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1kYI2BOTfi1NaUmzVpLWT0kQ_lh2g8WL24WV4bmdEjmw/edit">Short fiction by Jonas Lie</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1EA2U0gyDzA14gSH4vUsVYaFpKNik93TvoOOXQ-fklyg/edit">Short fiction by Katherine Mansfield</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1GDro-upVK4qg4jLtgHvHUU8QwfAqDzt0mevrVlG7nSY/edit">Short fiction by Mary de Morgan</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1ZbxSMsHQOlCohISZxyox3PQDefFnb0V5GeDwxXjZE4M/edit">Poetry by Paul Laurence Dunbar</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1JZwIq2GlV5Ly2xVCuPhVzYkM99mmRLeNrUcQHADBsAU/edit">Short fiction by Paul Laurence Dunbar</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1J9eXqZuq9P7HjZRsqJ8N-g5B4DM-Fi0PPWUBXW4AGOE/edit">Poetry by Georgia Douglas Johnson</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/document/d/1oKSdWxAypKpSzky1ToQ1j3_3FGD73DxPpD-4c_7v8S4/edit">Philosophical Works by René Descartes</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1KZHCdrtxXyCVNUPoujEH9xi36NH8vDXNMU0ofm4OJNg">Short fiction by Andre Norton</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/11rKzjg_XqnxNfK3x6tQFLzj0QLPLDEpR5_1AwQglao4">Short fiction by Manly Wade Wellman</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/16gEP0sxqUKFtkCz35pEKsE1G3gfBTg0kNKbEiR-dBnI">Short fiction by Richard Connell</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1KCml33TpVzDRd9Y3UrYdwefI61VNDKeqgRvC3Pug13E">Poetry by Robert Frost</a> (also see <a href="https://groups.google.com/d/msgid/standardebooks/8a231682-9e5a-485a-8e27-4562f3f1485fn%40googlegroups.com">this mailing list discussion</a>)</p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1MWoVEgZjol7UcCKUB1bQVdAAofLn1sAV8Yb9BlmttMs/edit">Poetry and short fiction by John Buchan</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1FeGDnuZ-d2Vlq8Fw29x5c3no5LvmNIIi3lP-e5Wq5tI/edit">Short fiction by Thomas Hardy</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/15dNXhPXRIAsGWsbPwFwlkly3FHKfEC9DYRB0-_uiooA/edit">Short fiction by H. G. Wells</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/10LSEBprE2yN1Mj__ImjqmMKQGZx4bJBBMwiQF0aOEFs/edit">Works by Edgar Saltus</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1zIrqVbb0nFh7wnOFCYQIUBUj0_G3GV79x9VSlhugZEE">Short Fiction by Fyodor Dostoevsky</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1SHbLqOdUgoodaBs_Mpv2KH25pr2jP-aiDVSbJSuxEjo">Short Fiction by Charles Beaumont</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1SHbLqOdUgoodaBs_Mpv2KH25pr2jP-aiDVSbJSuxEjo/edit?gid=0#gid=0">Short Fiction by Sarah Orne Jewett</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1wUReSh_dt9nWJzxDkYRKnVJVR0WZNK8ZA3wMtnjqqWA">Short Fiction by Mary Shelley</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1c7WFo60sBBkVCkyCTCWKRefL6SC6nucVkNIRSNXV3oU">Short Fiction by Bram Stoker</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1lObhHT6NBsL2mRjHOyFW46QFFqQt6pZYdzpL5TNrtmg">Short Fiction by Rabindranath Tagore</a></p>
-			</li>
-			<li>
-				<p><a href="https://docs.google.com/spreadsheets/d/1LCFPPSAiPlqPNuopdQyg-7-u78G92Y83M3hHXpjS-m4">Short Fiction by Saki</a></p>
-			</li>
-		</ul>
+		<section id="creating-a-new-spreadsheet">
+			<h2>Creating a new spreadsheet</h2>
+			<p>If you’d like to create a spreadsheet to catalog possible items to include in one or more omnibuses, please start by copying our <a href="https://docs.google.com/spreadsheets/d/1CE2AsQ7E5WS9NORubHuiVAmWHYDrmU3bXWUghmnU4kU">omnibus research template</a> by doing “File” → “Make a copy.”</p>
+			<p>The template includes a few sample items, which you must delete before you begin your own research. The columns should be self-explanatory.</p>
+			<ul>
+				<li>
+					<p>Change the title of the spreadsheet to something relevant to your research, like “Oscar Wilde Omnibuses.”</p>
+				</li>
+				<li>
+					<p>Include an item in the spreadsheet even if you feel that it doesn’t fit the S.E. collections policy. Leave the “Proposed omnibus” field blank.</p>
+				</li>
+				<li>
+					<p>Include the author’s entire corpus, even if some items aren’t in the U.S. public domain yet. This will help us continue adding to the omnibus over the years as items gradually enter the public domain. If an item isn’t in the public domain yet, it’ll be automatically greyed out.</p>
+				</li>
+				<li>
+					<p>You may add additional columns after “Notes,” but leave the basic columns intact.</p>
+				</li>
+				<li>
+					<p>Once you’ve actually added an item to an omnibus, check the “In omnibus?” field to indicate that it’s been included in a real S.E. omnibus and future producers no longer have to consider it.</p>
+				</li>
+			</ul>
+		</section>
+		<section id="spreadsheets">
+			<h2>Spreadsheets</h2>
+			<ul>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1hqz9yCVfPkw3IrOhkQASaPGy2oaTgNWKH1vmZd50384/edit#gid=0">Poetry by Langston Hughes</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/18rZtciwEVfuLTJJcCh-C3u41IZ7lpmcrP8-wQoMgOMs/edit">Essays by Errico Malatesta</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1kdLJRWsGUfgpFcuEXNi05BXdrB0sKsUrgszE8DCbEIY/edit">Works by P. G. Wodehouse</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1-lzAhqdAOG9n4myr-qzu3OiJMS2wqYmMfM8xvIIF6Us/edit">Poetry by Oscar Wilde</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1O1a33gw9qJ74NWj-YMBxPJ6Vany3hhGxIRRTqjJmVLw/edit">Works by J. Sheridan Le Fanu</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1vx_nljc4SzOW6LVzgCf411jVnA6XlT6Ga8EnZFTXGoQ/edit">Plays by G. B. Shaw</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/130sDHz3rFudKxenAPi_segf-4V0yTGaeVjTyYBv8P3M/edit">Works by Algernon Blackwood</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1lDcLirkqn2eK8Pp35UqhOemozD-5lBzz-nPZ14lymJc/edit">Plays by J. M. Synge</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1C-v7kjwZ0rbkp0C2pjInTh35d6jKA-BEoKvS500Agz8/edit">Works by Frederick Pohl</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/11RGuTztG5LzOfpOtchkjcuQhwkHShhE7PZL_bs4ygwg/edit">Works by H. P. Lovecraft</a> (<a href="https://docs.google.com/spreadsheets/d/11RGuTztG5LzOfpOtchkjcuQhwkHShhE7PZL_bs4ygwg/edit">Alternate spreadsheet</a>)</p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1R_fe5tNUhOmY38dnbA7n1DxJQO9KhHDwnzPgjT1ggac/edit">Works by Ambrose Bierce</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1IldpmxjK06BxPL4CCOEJZpw_bxm4AhYnrTchS03QP3k/edit">Works by Robert E. Howard</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1IldpmxjK06BxPL4CCOEJZpw_bxm4AhYnrTchS03QP3k/edit">Works by Ernest Hemingway</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1wTQTQjPDvAFDSWFyT5RY-Vw4Aop2jTs_BMIDbZXRUFs/edit">Poetry by G. K. Chesterton</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1KjbjEY3qz3pluw8cFPaKu-uUkCyb1aGuG6r4Xs_eT_E/edit">Short stories by W. S. Maugham</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1Li6i5qDxi2NZonsI7LM1C2RKPa-16-Hb8uguqyB8Lbk/edit">Works by Voltairine de Cleyre</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1xiWF5My5kCQJXrZ3NNYyUNMEMdZOQuKo-J5LGS_bVJE/edit">Poetry by Taras Shevchenko</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1Xso9v1FYk77TQgMh-NdYdO-r4WFfCwVHbDIngzNL-UM/edit">Works by Guy de Maupassant</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1yXZvlZVekVY3EpearMW8QIrbBEJlm8RbEXAuZP5rTG4/edit">Short fiction by Jack London</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1WB5Nt4LMVabt3KAUs1KxsfHfdpjU5OTs13dYITJcfLA/edit">Short fiction by Ray Bradbury</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1ArL5dfX30wa_ook1djY0AYyR191NFpv0U7IoAO2_c84/edit">Short fiction by Leo Tolstoy</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1vE0G9zslmxHZP17k529UiwPqSA9Zz8RBETtIHMA5Hqk/edit">Works by Thornton Burgess</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1rXHudjoiDoaWsS48KEw4lJWBVFVKkmhjqzaOKVB0IWQ/edit">Short fiction by Hans Christian Anderson</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1WkpEyYxPD21PbXH9yS5VZW1UcbhBNSnIFKJIS2wgA8A/edit">Short fiction by Rudyard Kipling</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1I4kXf3z2pmHTIVt7V3K57XiFmwof21o4uQOMNPK2oLo/edit">Short fiction by Sabine Baring-Gould</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1nLP-Ij0RTM15X8Xx6o_vV45GVdE2EiylCI2SRT2rN1Q/edit">Short fiction by E. F. Benson</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1ydCo8uwS4dJfSJoFgqIIAy9AuSfNcq0UhmKQ447-wY0/edit">Short fiction by Frank Belknap</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1jGIMLt2YaDT80312vNUfZBPbYxJBXHSCOYYtE85ILuk/edit">Short fiction by Nella Larsen</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1oYtEyX5ymsm8EN45LORUoh_WOKmiigu3GvMwSMzdizU/edit">Short fiction by Nelson S. Bond</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/17s4F5KlwHUODq87DUzEXWA0yAt7s2aeHBV52iDWpAsI/edit">Short fiction by F. Scott Fitzgerald</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1kYI2BOTfi1NaUmzVpLWT0kQ_lh2g8WL24WV4bmdEjmw/edit">Short fiction by Jonas Lie</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1EA2U0gyDzA14gSH4vUsVYaFpKNik93TvoOOXQ-fklyg/edit">Short fiction by Katherine Mansfield</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1GDro-upVK4qg4jLtgHvHUU8QwfAqDzt0mevrVlG7nSY/edit">Short fiction by Mary de Morgan</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1ZbxSMsHQOlCohISZxyox3PQDefFnb0V5GeDwxXjZE4M/edit">Poetry by Paul Laurence Dunbar</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1JZwIq2GlV5Ly2xVCuPhVzYkM99mmRLeNrUcQHADBsAU/edit">Short fiction by Paul Laurence Dunbar</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1J9eXqZuq9P7HjZRsqJ8N-g5B4DM-Fi0PPWUBXW4AGOE/edit">Poetry by Georgia Douglas Johnson</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/document/d/1oKSdWxAypKpSzky1ToQ1j3_3FGD73DxPpD-4c_7v8S4/edit">Philosophical Works by René Descartes</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1KZHCdrtxXyCVNUPoujEH9xi36NH8vDXNMU0ofm4OJNg">Short fiction by Andre Norton</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/11rKzjg_XqnxNfK3x6tQFLzj0QLPLDEpR5_1AwQglao4">Short fiction by Manly Wade Wellman</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/16gEP0sxqUKFtkCz35pEKsE1G3gfBTg0kNKbEiR-dBnI">Short fiction by Richard Connell</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1KCml33TpVzDRd9Y3UrYdwefI61VNDKeqgRvC3Pug13E">Poetry by Robert Frost</a> (also see <a href="https://groups.google.com/d/msgid/standardebooks/8a231682-9e5a-485a-8e27-4562f3f1485fn%40googlegroups.com">this mailing list discussion</a>)</p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1MWoVEgZjol7UcCKUB1bQVdAAofLn1sAV8Yb9BlmttMs/edit">Poetry and short fiction by John Buchan</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1FeGDnuZ-d2Vlq8Fw29x5c3no5LvmNIIi3lP-e5Wq5tI/edit">Short fiction by Thomas Hardy</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/15dNXhPXRIAsGWsbPwFwlkly3FHKfEC9DYRB0-_uiooA/edit">Short fiction by H. G. Wells</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/10LSEBprE2yN1Mj__ImjqmMKQGZx4bJBBMwiQF0aOEFs/edit">Works by Edgar Saltus</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1zIrqVbb0nFh7wnOFCYQIUBUj0_G3GV79x9VSlhugZEE">Short Fiction by Fyodor Dostoevsky</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1SHbLqOdUgoodaBs_Mpv2KH25pr2jP-aiDVSbJSuxEjo">Short Fiction by Charles Beaumont</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1SHbLqOdUgoodaBs_Mpv2KH25pr2jP-aiDVSbJSuxEjo/edit?gid=0#gid=0">Short Fiction by Sarah Orne Jewett</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1wUReSh_dt9nWJzxDkYRKnVJVR0WZNK8ZA3wMtnjqqWA">Short Fiction by Mary Shelley</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1c7WFo60sBBkVCkyCTCWKRefL6SC6nucVkNIRSNXV3oU">Short Fiction by Bram Stoker</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1lObhHT6NBsL2mRjHOyFW46QFFqQt6pZYdzpL5TNrtmg">Short Fiction by Rabindranath Tagore</a></p>
+				</li>
+				<li>
+					<p><a href="https://docs.google.com/spreadsheets/d/1LCFPPSAiPlqPNuopdQyg-7-u78G92Y83M3hHXpjS-m4">Short Fiction by Saki</a></p>
+				</li>
+			</ul>
+		</section>
 	</article>
 </main>
 <?= Template::Footer() ?>
