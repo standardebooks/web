d2f963e5ebca17a90371306195ada6ab3677a697
Alex Cabal <alex@alexcabal.com>
2026-01-22 14:43:03 -0600

    Set default value for TitleInCollection

--- lib/CollectionMembership.php
+++ lib/CollectionMembership.php
@@ -11,7 +11,7 @@ class CollectionMembership{
 	public int $SortOrder;
 	/** If an `Ebook` is listed in a collection under an alternate title, then this value will not be `NULL`.
 	 * For example, a collection like the Haycraft-Queen Cornerstones might have an entry that is a short story, but that we have in the corpus as part of a larger omnibus. `$TitleInCollection` can be set to the short story name to show that, intead of the omnibus name, when listing the collection. */
-	public ?string $TitleInCollection;
+	public ?string $TitleInCollection = null;
 
 	protected Collection $_Collection;
 }
