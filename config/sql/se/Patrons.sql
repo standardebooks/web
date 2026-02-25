e6c7c094241e482fca077896b91c99b51af8c5cd
Mike Colagrosso <colagrosso@users.noreply.github.com>
2026-02-24 21:38:09 -0700

    Correct default `BaseCost` line in `Patrons`

--- config/sql/se/Patrons.sql
+++ config/sql/se/Patrons.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `Patrons` (
   `UserId` int(10) unsigned NOT NULL,
   `IsAnonymous` tinyint(1) unsigned NOT NULL DEFAULT 0,
   `AlternateName` varchar(80) DEFAULT NULL,
-  `BaseCost` DECIMAL(5,2) UNSIGNED NULL DEFAULT,
+  `BaseCost` DECIMAL(5,2) UNSIGNED NULL DEFAULT NULL,
   `CycleType` enum('monthly','yearly','unlimited') NULL DEFAULT NULL,
   `Created` datetime NOT NULL,
   `Ended` datetime DEFAULT NULL,
