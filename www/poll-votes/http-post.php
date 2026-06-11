7e0b92e80fcd395c9ccbe14c7fba9f299c3eb062
Alex Cabal <alex@alexcabal.com>
2026-06-11 12:54:04 -0500

    Don't allow voting in inactive polls

--- www/poll-votes/http-post.php
+++ www/poll-votes/http-post.php
@@ -11,7 +11,7 @@ try{
 		throw new Exceptions\LoginRequiredException();
 	}
 
-	if(!Session::$User->Benefits->CanVote){
+	if(!Session::$User->Benefits->CanVote || !$poll->IsActive()){
 		throw new Exceptions\PermissionsInvalidException();
 	}
 
