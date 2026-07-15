cf1c2e80477300d9c898dee79cffb58e846ad800
Alex Cabal <alex@alexcabal.com>
2026-07-15 15:18:14 -0500

    Add hero image to blog index

--- www/blog-posts/index.php
+++ www/blog-posts/index.php
@@ -8,8 +8,13 @@ else{
 ?>
 <?= Template::Header(title: 'Blog', description: 'The Standard Ebooks blog.') ?>
 <main>
-	<section class="narrow">
+	<section class="has-hero">
 		<h1>Blog</h1>
+		<picture data-caption="Girl in a Hammock. Winslow Homer, 1873">
+			<source srcset="/images/girl-in-a-hammock@2x.avif 2x, /images/girl-in-a-hammock.avif 1x" type="image/avif"/>
+			<source srcset="/images/girl-in-a-hammock@2x.jpg 2x, /images/girl-in-a-hammock.jpg 1x" type="image/jpeg"/>
+			<img src="/images/girl-in-a-hammock@2x.jpg" alt="A girl reclines in a hammock and reads a book."/>
+		</picture>
 		<? if(Session::$User?->Benefits->CanEditBlogPosts){ ?>
 			<ul role="menu">
 				<li><a href="/blog-posts/new">Create a blog post</a></li>
