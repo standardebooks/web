c90a221e892ebeb9d052f97188a32d452656c8fe
Alex Cabal <alex@alexcabal.com>
2026-07-15 16:39:13 -0500

    Style blog front page

--- www/blog-posts/index.php
+++ www/blog-posts/index.php
@@ -6,7 +6,7 @@ else{
 	$blogPosts = BlogPost::GetAllByIsPublished();
 }
 ?>
-<?= Template::Header(title: 'Blog', description: 'The Standard Ebooks blog.') ?>
+<?= Template::Header(title: 'Blog', description: 'The Standard Ebooks blog.', css: ['/css/blog.css']) ?>
 <main>
 	<section class="has-hero">
 		<h1>Blog</h1>
@@ -20,15 +20,37 @@ else{
 				<li><a href="/blog-posts/new">Create a blog post</a></li>
 			</ul>
 		<? } ?>
-		<ul>
-			<? foreach($blogPosts as $blogPost){ ?>
-				<li>
-					<p>
-						<a href="<?= $blogPost->Url ?>"><?= $blogPost->Title ?></a><? if(Session::$User?->Benefits->CanEditBlogPosts){ ?> • <? if($blogPost->Body === null){ ?>Must be edited in the database<? }else{ ?><a href="<?= $blogPost->EditUrl ?>">Edit</a><? } ?><? } ?>
-					</p>
-				</li>
-			<? } ?>
-		</ul>
+		<? if(Session::$User?->Benefits->CanEditBlogPosts){ ?>
+			<ul>
+				<? foreach($blogPosts as $blogPost){ ?>
+					<li>
+						<p>
+							<a href="<?= $blogPost->Url ?>"><?= $blogPost->Title ?></a> • <? if($blogPost->Body === null){ ?>Must be edited in the database<? }else{ ?><a href="<?= $blogPost->EditUrl ?>">Edit</a><? } ?>
+						</p>
+					</li>
+				<? } ?>
+			</ul>
+		<? }else{ ?>
+			<ul class="blog-post-list">
+				<? foreach($blogPosts as $blogPost){ ?>
+					<li>
+						<a class="blog-post-card<? if($blogPost->ImageCacheKey === null){ ?> no-hero-image<? } ?>" href="<?= $blogPost->Url ?>">
+							<? if($blogPost->ImageCacheKey !== null){ ?>
+								<picture>
+									<source srcset="<?= $blogPost->HeroImageAvifUrl ?> 1x, <?= $blogPost->HeroImageAvif2xUrl ?> 2x" type="image/avif" />
+									<source srcset="<?= $blogPost->HeroImageUrl ?> 1x, <?= $blogPost->HeroImage2xUrl ?> 2x" type="image/jpeg" />
+									<img src="<?= $blogPost->HeroImageUrl ?>" alt="" />
+								</picture>
+							<? } ?>
+							<span class="blog-post-card-label">
+								<span class="blog-post-card-title"><?= $blogPost->Title ?></span>
+								<span class="blog-post-card-byline">By <?= Formatter::EscapeHtml($blogPost->User->Name) ?></span>
+							</span>
+						</a>
+					</li>
+				<? } ?>
+			</ul>
+		<? } ?>
 	</section>
 </main>
 <?= Template::Footer() ?>
