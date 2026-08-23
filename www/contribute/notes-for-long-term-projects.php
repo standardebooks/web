d00498c21b7cdcfac8b9e9cc824ac564fe875b99
Alex Cabal <alex@alexcabal.com>
2026-08-23 16:17:52 -0500

    Add book to long term projects

--- www/contribute/notes-for-long-term-projects.php
+++ www/contribute/notes-for-long-term-projects.php
@@ -7,7 +7,7 @@
 			<h2>Projects for 2029<a class="heading-permalink" href="#projects-for-2029" aria-label="Permalink"></a></h2>
 			<ul>
 				<li>
-					<p>Kant’s <i>Critique of Pure Reason</i>: Norman Kemp Smith’s translation is desired. It was published in 1929, but heavily revised in 1933; therefore we must wait until PD 1933 to produce the revision. See <a href="https://groups.google.com/d/msgid/standardebooks/68617ce4-690b-48f7-ad2f-6a805e4c3e6bn%40googlegroups.com?utm_medium=email&amp;utm_source=footer">this discussion thread</a>.</p>
+					<p><i>Critique of Pure Reason</i> by Immanuel Kant, translated by Norman Kemp Smith: Smith’s translation is desired. It was published in 1929, but heavily revised in 1933; therefore we must wait until PD 1933 to produce the revision. See <a href="https://groups.google.com/d/msgid/standardebooks/68617ce4-690b-48f7-ad2f-6a805e4c3e6bn%40googlegroups.com?utm_medium=email&amp;utm_source=footer">this discussion thread</a>.</p>
 				</li>
 			</ul>
 		</section>
@@ -16,6 +16,14 @@
 			<ul>
 				<li>
 					<p><i><a href="https://standardebooks.org/ebooks/edgar-rice-burroughs/pirates-of-venus">Pirates of Venus</a></i> should have its cover image updated to match the first book edition dust jacket cover by J. Allen St. John, which was published in 1934.</p>
+				</li>
+			</ul>
+		</section>
+		<section id="projects-for-2031">
+			<h2>Projects for 2031<a class="heading-permalink" href="#projects-for-2031" aria-label="Permalink"></a></h2>
+			<ul>
+				<li>
+					<p><i>The Cossacks</i> by Leo Tolstoy, translated by Louise Maude and Aylmer Maude: The PG transcription is of the 1935 Maude translation, which differs from the 1916 Maude translation. Therefore we must wait until PD 1935. Beginner difficulty.</p>
 				</li>
 			</ul>
 		</section>
